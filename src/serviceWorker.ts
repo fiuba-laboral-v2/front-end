b1db8fb3833ecda4a4272cd77bf1c718152de4f2
dylan <dylanalvarez1995@gmail.com>
2019-12-31 15:02:42 -0300

    Reference shared tslint config

--- src/serviceWorker.ts
+++ src/serviceWorker.ts
@@ -16,8 +16,8 @@ const isLocalhost = Boolean(
     window.location.hostname === "[::1]" ||
     // 127.0.0.0/8 are considered localhost for IPv4.
     window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/,
-    ),
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
 );
 
 interface IConfig {
@@ -40,10 +40,6 @@ const registerValidSW = (swUrl: string, config?: IConfig) => {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
               // content until all client tabs are closed.
-              console.log(
-                "New content is available and will be used when all " +
-                  "tabs for this page are closed. See https://bit.ly/CRA-PWA.",
-              );
 
               // Execute callback
               if (config && config.onUpdate) {
@@ -53,7 +49,6 @@ const registerValidSW = (swUrl: string, config?: IConfig) => {
               // At this point, everything has been precached.
               // It's the perfect time to display a
               // "Content is cached for offline use." message.
-              console.log("Content is cached for offline use.");
 
               // Execute callback
               if (config && config.onSuccess) {
@@ -64,15 +59,15 @@ const registerValidSW = (swUrl: string, config?: IConfig) => {
         };
       };
     })
-    .catch(error => {
-      console.error("Error during service worker registration:", error);
+    .catch(() => {
+      // Error during service worker registration
     });
 };
 
 const checkValidServiceWorker = (swUrl: string, config?: IConfig) => {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl, {
-    headers: { "Service-Worker": "script" },
+    headers: { "Service-Worker": "script" }
   })
     .then(response => {
       // Ensure service worker exists, and that we really are getting a JS file.
@@ -93,9 +88,7 @@ const checkValidServiceWorker = (swUrl: string, config?: IConfig) => {
       }
     })
     .catch(() => {
-      console.log(
-        "No internet connection found. App is running in offline mode.",
-      );
+      // No internet connection found. App is running in offline mode.
     });
 };
 
@@ -104,7 +97,7 @@ const register = (config?: IConfig) => {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(
       process.env.PUBLIC_URL,
-      window.location.href,
+      window.location.href
     );
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
@@ -123,10 +116,8 @@ const register = (config?: IConfig) => {
         // Add some additional logging to localhost, pointing developers to the
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
-          console.log(
-            "This web app is being served cache-first by a service " +
-              "worker. To learn more, visit https://bit.ly/CRA-PWA",
-          );
+          // This web app is being served cache-first by a service
+          // worker. To learn more, visit https://bit.ly/CRA-PWA
         });
       } else {
         // Is not localhost. Just register service worker
