386aa61fd459dbbbbd3f4360da71e5d9d11b90f7
Sebastian Blanco <sblanco@devartis.com>
2020-01-15 03:40:02 -0300

    Add message in case of failure

--- scripts/apache/deploy.sh
+++ scripts/apache/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ~./fiuba-laboral-v2
+cd ~./fiuba-laboral-v2  || echo "DIRECTORY fiuba-laboral-v2 DOES NOT EXIST" && exit
 git clone git@github.com:fiuba-laboral-v2/front-end.git
-cd front-end
+cd front-end  || echo "DIRECTORY front-end DOES NOT EXIST" && exit
 yarn deploy:up
