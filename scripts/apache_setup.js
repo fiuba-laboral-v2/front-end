00c7daeaa2928205bb9e4a03c0f184ae84ce34c4
Sebastian Blanco <sblanco@devartis.com>
2020-01-19 15:54:36 -0300

    Add contibutors and docker pemissions setup

--- scripts/apache_setup.js
+++ scripts/apache_setup.js
@@ -15,6 +15,12 @@ shell.echo(`creating user permissions for the "/var/www/${hostname}/html" folder
 shell.exec(`sudo chown -R "$USER:$USER" "/var/www/${hostname}/html"`);
 shell.exec(`sudo chmod -R 755 "/var/www/${hostname}"`);
 
+shell.echo("creating apache cofiguration");
 shell.exec(`HOSTNAME=${hostname} bash scripts/set_default_settings.sh`);
+
+shell.echo("Set up docker permissions");
+shell.exec("sudo groupadd docker");
+shell.exec("sudo usermod -aG docker $USER");
+shell.exec("sudo usermod -aG docker $USER");
 
 shell.exec(`node scripts/deploy.js`);
