8d8dbd69d2efcd9d93498ae421a096432993c894
Sebastian Blanco <sblanco@devartis.com>
2020-01-18 21:56:24 -0300

    Test deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,7 +10,6 @@ const repository = config.repository;
 const branch = config.branch;
 const publicURL = config.PUBLIC_URL;
 const sshAddress = config.ssh_address;
-shell.exec("yarn install");
 shell.echo("$PASSWORD");
 shell.exec("sshpass -p $PASSWORD ssh laboral@antiguos.fi.uba.ar 'touch test_file'");
 // shell.exec(`ssh ${sshAddress} rm -rf ${location}`);
