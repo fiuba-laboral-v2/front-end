74eb80ecfe26b2460da6639ca9014a8740cea6cd
Sebastian Blanco <sblanco@devartis.com>
2020-01-18 22:37:51 -0300

    Test deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,7 +10,7 @@ const repository = config.repository;
 const branch = config.branch;
 const publicURL = config.PUBLIC_URL;
 const sshAddress = config.ssh_address;
-shell.echo("password: $PASSWORD");
+shell.echo(`password: ${process.env.PASSWORD}`);
 shell.exec("sshpass -p $PASSWORD ssh laboral@antiguos.fi.uba.ar 'touch test_file'");
 // shell.exec(`ssh ${sshAddress} rm -rf ${location}`);
 // shell.exec(`ssh ${sshAddress} git clone -b ${branch} ${repository} ${location}`);
