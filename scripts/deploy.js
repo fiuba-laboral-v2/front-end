05e823699d6de456c280f2be120f6641f163b89e
Sebastian Blanco <sblanco@devartis.com>
2020-01-19 03:13:30 -0300

    Test travis env var

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -10,7 +10,7 @@ const repository = config.repository;
 const branch = config.branch;
 const publicURL = config.PUBLIC_URL;
 const sshAddress = config.ssh_address;
-// shell.exec(`ssh laboral@antiguos.fi.uba.ar 'touch test_file'`);
+shell.exec(`ssh laboral@antiguos.fi.uba.ar 'touch test_file'`);
 // shell.exec(`ssh ${sshAddress} rm -rf ${location}`);
 // shell.exec(`ssh ${sshAddress} git clone -b ${branch} ${repository} ${location}`);
 // shell.exec(`ssh ${sshAddress} HOSTNAME=${hostname} PUBLIC_URL=${publicURL} bash ${location}/scripts/load_html.sh`);
