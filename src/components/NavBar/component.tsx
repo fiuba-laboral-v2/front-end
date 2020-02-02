95c2e0011b217e28fbae423e58b53983c5add154
dylan <dylanalvarez1995@gmail.com>
2020-02-02 15:35:15 -0300

    Make navbar responsive

--- src/components/NavBar/component.tsx
+++ src/components/NavBar/component.tsx
@@ -1,5 +1,6 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { Link } from "react-router-dom";
+import MenuIcon from "@material-ui/icons/Menu";
 
 import styles from "./styles.module.scss";
 
@@ -17,17 +18,34 @@ const NavBar: FunctionComponent<INavBarProps> = (
     logOut,
     username
   }
-) => (
-  <div className={styles.navBar}>
-    <div className={styles.navigation}>
-      <Link to="/">{title}</Link>
-      <Link to="/my-company/">{myCompanyProfile}</Link>
+) => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  return (
+    <div className={styles.navBar}>
+      <div className={styles.main}>
+        <Link to="/">{title}</Link>
+        <div className={styles.toggle}>
+          <div className={styles.separator}/>
+          <div className={styles.menuIcon} onClick={toggleMenu}>
+            <MenuIcon/>
+          </div>
+        </div>
+      </div>
+      <div className={`${styles.menu} ${showMenu && styles.showOnMobile}`}>
+        <Link to="/my-company/">{myCompanyProfile}</Link>
+        <div className={styles.separator}/>
+        <div className={styles.user}>
+          <p>{username}</p>
+          <Link to="/">{logOut}</Link>
+        </div>
+      </div>
     </div>
-    <div>
-      <p>{username}</p>
-      <Link to="/">{logOut}</Link>
-    </div>
-  </div>
-);
+  );
+};
 
 export default NavBar;
