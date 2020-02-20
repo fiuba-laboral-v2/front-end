6a25f91bbc08656222219b142f43663d49ff6fc7
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-02-20 00:16:52 -0300

    Remove redundant divs

--- src/components/ApplicantDetail/component.tsx
+++ src/components/ApplicantDetail/component.tsx
@@ -17,42 +17,40 @@ const ApplicantDetail: FunctionComponent<IApplicantDetailProps> = (
     translations
   }) => (
     <DetailMainContainer detailBody={
-        <div className={styles.mainContainer}>
-            <div className={styles.detailContainer}>
-                <div className={styles.header}>
-                    <div className={styles.fullNameContainer}>
-                        <DetailHeadline headline={`${name} ${surname}`}/>
-                    </div>
-                    <div className={styles.padronContainer}>
-                        <span className={styles.padronTitle}>{translations.padron}:</span>
-                        <DetailByLine byLine={padron}/>
-                    </div>
-                    <div className={styles.descriptionContainer}>
-                        <DetailDescription  description={description}/>
-                    </div>
+        <div>
+            <div className={styles.header}>
+                <div className={styles.fullNameContainer}>
+                    <DetailHeadline headline={`${name} ${surname}`}/>
                 </div>
-                <div className={styles.info}>
-                    <section className={styles.capabilities}>
+                <div className={styles.padronContainer}>
+                    <span className={styles.padronTitle}>{translations.padron}:</span>
+                    <DetailByLine byLine={padron}/>
+                </div>
+                <div className={styles.descriptionContainer}>
+                    <DetailDescription  description={description}/>
+                </div>
+            </div>
+            <div className={styles.info}>
+                <section className={styles.capabilities}>
                         <span
                           className={styles.capabilityTitle}> {translations.capabilities}:
                         </span>
-                        {
-                            capabilities?.map(capability =>
-                              (<span className={styles.capability}>{capability}</span>))
-                        }
-                    </section>
-                    <section className={styles.careers}>
-                        <span className={styles.careerTitle}> { translations.careers }: </span>
-                        {
-                            careers?.map(career =>
-                                (
-                                  <span className={styles.career}>
+                    {
+                        capabilities?.map(capability =>
+                          (<span className={styles.capability}>{capability}</span>))
+                    }
+                </section>
+                <section className={styles.careers}>
+                    <span className={styles.careerTitle}> { translations.careers }: </span>
+                    {
+                        careers?.map(career =>
+                            (
+                              <span className={styles.career}>
                             {career.name} - {career.credits} {translations.credits}
                         </span>)
-                            )
-                        }
-                    </section>
-                </div>
+                        )
+                    }
+                </section>
             </div>
         </div>
     }/>
