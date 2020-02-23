b5cf2e159cc5d1a0841bd3b565b0c6c7dfcf602c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-02-22 22:01:02 -0300

    Place single argument inline

--- src/components/Detail/DetailMainContainer/component.tsx
+++ src/components/Detail/DetailMainContainer/component.tsx
@@ -1,11 +1,7 @@
 import React, { FunctionComponent } from "react";
 import styles from "./styles.module.scss";
 
-const DetailMainContainer: FunctionComponent = (
-  {
-    children
-  }
-) => (
+const DetailMainContainer: FunctionComponent = ({ children}) => (
   <div className={styles.mainContainer}>
     <div className={styles.detailContainer}>
       {children}
