1931b056f57125888bced66bf291b9ff4e370bb8
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-02-25 13:23:35 -0300

    Remove query for temporary implace translation on Applicant editable detail title

--- src/components/ApplicantDetailEditableTitle/container.tsx
+++ src/components/ApplicantDetailEditableTitle/container.tsx
@@ -1,22 +1,13 @@
 import React, { FunctionComponent } from "react";
-import { useQuery } from "@apollo/react-hooks";
 import { useParams } from "react-router-dom";
-import { getTranslations } from "$queries";
 import { DetailTitle } from "../Detail/DetailTitle";
 
 const ApplicantDetailEditableTitleContainer: FunctionComponent = () => {
-  const { data } = useQuery(getTranslations, {
-      variables: {
-        paths: ["applicant.explanation", "applicant.title", "edit"]
-      }
-    }
-  );
-  const [explanation, title, edit] = data ? data.getTranslations : ["", "", ""];
   const { id } = useParams();
 
   return (
     <DetailTitle
-      myDetail={title}
+      myDetail={"Mi perfil"}
       explanation={"Ahora puede editar su perfil"}
       edit={"Editando"}
       link={`/applicants/${id}`}
