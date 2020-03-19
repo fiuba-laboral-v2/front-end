776c6ca64d531ab87220bc82e4041704d5f31ee7
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-03-18 23:05:40 -0300

    Rename company profile components to avoid redundancy

--- src/pages/Company/CompanyTitle/index.tsx
+++
@@ -1,3 +0,0 @@
-import CompanyTitle from "./container";
-
-export default CompanyTitle;
--- src/pages/Company/index.tsx
+++ src/pages/Company/index.tsx
@@ -1,14 +1,14 @@
 import React, { FunctionComponent } from "react";
 import NavBar from "$components/NavBar";
-import CompanyTitle from "./CompanyTitle";
-import { CompanyDetail } from "./CompanyDetail";
+import { Title } from "./Profile/Title";
+import { Detail } from "./Profile/Detail";
 
 const Company: FunctionComponent = () => (
   <div>
     <NavBar/>
     <div className="mainContent">
-      <CompanyTitle/>
-      <CompanyDetail/>
+      <Title/>
+      <Detail/>
     </div>
   </div>
 );
