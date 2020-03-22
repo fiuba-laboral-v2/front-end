d5269fa2b42bca854b1509a7b320add75c5b1616
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-03-22 03:56:58 -0300

    Add title to applicant list

---
+++ src/pages/Applicant/List/Title/container.tsx
@@ -0,0 +1,26 @@
+import React, { FunctionComponent } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import { getTranslations } from "$queries";
+import { Title } from "$components/Title";
+import NotFound from "$pages/NotFound";
+import Loading from "$pages/Loading";
+
+const TitleContainer: FunctionComponent = () => {
+  const translationsResponse = useQuery(getTranslations, {
+      variables: {
+        paths: ["applicant.title"]
+      }
+    }
+  );
+  if (translationsResponse.error) return <NotFound/>;
+  if (translationsResponse.loading) return <Loading />;
+
+  return (
+    <Title
+      title={"Postulantes"}
+      subtitle={""}
+    />
+  );
+};
+
+export { TitleContainer };
--- src/pages/Applicant/List/index.tsx
+++ src/pages/Applicant/List/index.tsx
@@ -1,11 +1,13 @@
 import React, { FunctionComponent } from "react";
 import NavBar from "$components/NavBar";
 import { Applicants } from "./Applicants";
+import { Title } from "./Title";
 
 const List: FunctionComponent = () => (
   <div>
     <NavBar />
     <div className="mainContent">
+      <Title/>
       <Applicants />
     </div>
   </div>
