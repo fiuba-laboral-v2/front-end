5e228b53bd4ec67af2f18b71d9dab4c9750ad5b0
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-03-22 17:52:52 -0300

    Replace div with fragment

--- src/pages/Applicant/List/Applicants/component.tsx
+++ src/pages/Applicant/List/Applicants/component.tsx
@@ -10,7 +10,7 @@ const Applicants: FunctionComponent<IApplicantsProps> = (
     editButtonText,
     viewButtonText
   }) => (
-  <div>
+  <>
     {
       applicants.map(applicant =>
         <ListItem>
@@ -20,7 +20,7 @@ const Applicants: FunctionComponent<IApplicantsProps> = (
         </ListItem>
       )
     }
-  </div>
+  </>
 );
 
 
