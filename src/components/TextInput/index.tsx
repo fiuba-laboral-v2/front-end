40374fb571d3a7aeb36e29b0a19f8cfb72100d05
dylan <dylanalvarez1995@gmail.com>
2020-03-24 16:25:19 -0300

    Restore conditional rendering of error message in TextInput

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -8,14 +8,15 @@ import styles from "./styles.module.scss";
 const TextInput: FunctionComponent<IInputProps> = ({ label, ...props }) => {
   const [field, meta] = useField(props.name);
   const containerClassNames = [styles.inputContainer];
-  if (meta.touched && meta.error) containerClassNames.push(styles.error);
+  const error = meta.touched && meta.error;
+  if (error) containerClassNames.push(styles.error);
   if (!props.placeholder) props.placeholder = label;
 
   return (
     <div className={classNames(...containerClassNames)}>
       <label className={styles.labelInput} htmlFor={props.name}>{label}</label>
       <input className={styles.textInput} {...field} {...props} />
-      <div className={styles.errorMessage}>{meta.error}</div>
+      {error && <div className={styles.errorMessage}>{meta.error}</div>}
     </div>
   );
 };
