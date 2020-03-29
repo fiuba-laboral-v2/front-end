f2cbe64f087a3d8a5ca0abd8fda58c7d4c608827
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-03-28 23:23:54 -0300

    De-structure the links in name and url

--- src/components/Links/component.tsx
+++ src/components/Links/component.tsx
@@ -5,9 +5,9 @@ import styles from "./styles.module.scss";
 const Links: FunctionComponent<ILinksProps> = ({ links, className }) => (
     <div className={`${styles.links} ${className}`}>
       {
-        links.map((link, index) =>
+        links.map(({ url, name }, index) =>
           <div key={index} className={styles.link}>
-            <a target="_blank" rel="noopener noreferrer" href={link.url}>{link.name}</a>
+            <a target="_blank" rel="noopener noreferrer" href={url}>{name}</a>
             <span className={styles.divider}> — </span>
           </div>
         )
