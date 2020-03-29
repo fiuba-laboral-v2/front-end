7cda55072ff5a0bc429af92eebe0884a2f79fb51
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-03-29 18:00:23 -0300

    Remove subtitle mixin and move style to Subtitle component

--- src/components/Links/component.tsx
+++ src/components/Links/component.tsx
@@ -1,18 +1,21 @@
 import React, { FunctionComponent } from "react";
+import { Subtitle } from "$components/Subtitle";
 import { ILinksProps } from "./interface";
 import styles from "./styles.module.scss";
 
 const Links: FunctionComponent<ILinksProps> = ({ links, className }) => (
-    <div className={`${styles.links} ${className}`}>
-      {
-        links.map(({ url, name }, index) =>
-          <div key={index} className={styles.link}>
+  <div className={`${styles.links} ${className}`}>
+    {
+      links.map(({ url, name }, index) =>
+        <div key={index} className={styles.link}>
+          <Subtitle>
             <a target="_blank" rel="noopener noreferrer" href={url}>{name}</a>
-            <span className={styles.divider}> — </span>
-          </div>
-        )
-      }
-    </div>
+          </Subtitle>
+          <span className={styles.divider}> — </span>
+        </div>
+      )
+    }
+  </div>
 );
 
 export { Links };
