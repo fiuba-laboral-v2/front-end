d2ac8afa299508cafc308128bf28f4a2e34eb18c
dylan <dylanalvarez1995@gmail.com>
2020-03-30 19:18:05 -0300

    Move translation to its Page

--- src/components/CareerSelector/container.tsx
+++ src/components/CareerSelector/container.tsx
@@ -7,10 +7,12 @@ import { getTranslations } from "../../graphql/queries";
 
 export const CareerSelectorContainer: FunctionComponent<ICareerSelectorContainerProps> = props => {
   const { data: translationsData, loading, error } = useQuery(getTranslations, {
-      variables: { paths: ["applicant.career", "applicant.signUp.credits"] }
-    }
-  );
-  const [careerLabel, creditsLabel] = translationsData ? translationsData.getTranslations : [""];
+    variables: { paths: ["applicant.signUp.career", "applicant.signUp.credits"] }
+  });
+  const [
+    careerLabel,
+    creditsLabel
+  ] = translationsData ? translationsData.getTranslations : ["", ""];
 
   if (loading || error) return (<div/>);
 
