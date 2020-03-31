f7c27f4aada2dd38135351d55a1a8cda3716f1f1
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-03-30 23:13:06 -0300

    MAke home redirects to applicants list page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,9 @@
 import React, { FunctionComponent } from "react";
-import { List } from "$pages/Applicant/List";
+import { Redirect } from "react-router-dom";
+import { RoutesBuilder } from "$src/routesBuilder";
 
 const Home: FunctionComponent = () => (
-  <List />
+  <Redirect to={RoutesBuilder.applicant.list()}/>
 );
 
 export default Home;
