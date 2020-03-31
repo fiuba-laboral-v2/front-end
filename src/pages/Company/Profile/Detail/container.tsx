09e8a7d4f50d472108e6fba330bd92fa1caa16e8
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-03-30 21:45:08 -0300

    Add loading to company profile page

--- src/pages/Company/Profile/Detail/container.tsx
+++ src/pages/Company/Profile/Detail/container.tsx
@@ -1,52 +1,21 @@
 import React, { FunctionComponent } from "react";
-import { Detail } from "./component";
-import { getCompanyById } from "$queries";
+import { getCompanyById as GET_COMPANY_BY_ID } from "$queries";
 import { useQuery } from "@apollo/react-hooks";
 import { useParams } from "react-router-dom";
+import { ICompany } from "$interfaces/Company";
+import { Detail } from "./component";
 import NotFound from "$pages/NotFound";
 
 const DetailContainer: FunctionComponent = () => {
   const { id } = useParams();
-  const response = useQuery(getCompanyById, {
-        variables: {
-          id: id
-        }
-      }
-  );
-
-  if (response.error) {
-    return (<NotFound/>);
-  }
-
   const {
-    companyName,
-    slogan,
-    logo,
-    description,
-    photos,
-    website,
-    email
-  } = response.data ? response.data.getCompanyById : {
-    companyName: "",
-    slogan: "",
-    logo: "",
-    description: "",
-    website: "",
-    email: "",
-    photos: []
-  };
+    data: { getCompanyById } = { getCompanyById: {} as ICompany },
+    error,
+    loading
+  } = useQuery(GET_COMPANY_BY_ID, { variables: { id: id } });
+  if (error) return <NotFound />;
 
-  return (
-    <Detail
-      name={companyName}
-      email={email}
-      slogan={slogan}
-      logoImageSource={logo}
-      website={website}
-      description={description}
-      photoImageSources={photos}
-    />
-  );
+  return <Detail loading={loading} company={getCompanyById} />;
 };
 
 export { DetailContainer };
