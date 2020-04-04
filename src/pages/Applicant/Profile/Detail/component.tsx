9111a31c3a869bf3dca597b9110f796b6f07431a
dylan <dylanalvarez1995@gmail.com>
2020-04-03 22:12:32 -0300

    Move loading div into applicant profile detail container

--- src/pages/Applicant/Profile/Detail/component.tsx
+++ src/pages/Applicant/Profile/Detail/component.tsx
@@ -1,6 +1,4 @@
 import React, { FunctionComponent } from "react";
-import { IApplicantDetailProps } from "./interface";
-import { LoadingSpinner } from "$components/LoadingSpinner";
 import { DetailHeadline } from "$components/Detail/DetailHeadline";
 import { Links } from "$components/Links";
 import { CapabilitiesDetail } from "$pages/Applicant/Profile/CapabilitiesDetail";
@@ -8,42 +6,42 @@ import { CareersDetail } from "$pages/Applicant/Profile/CareersDetail";
 import { SectionDetail } from "$pages/Applicant/Profile/SectionDetail";
 
 import styles from "./styles.module.scss";
+import { IApplicant } from "$interfaces/Applicant";
 
 const Detail: FunctionComponent<IApplicantDetailProps> = (
   {
     applicant,
-    translations,
-    loading
-  }) => {
-  if (loading) {
-    return (
-      <div className={styles.container}>
-        <LoadingSpinner/>
-      </div>
-    );
-  }
-
-  return (
-    <div className={styles.container}>
-      <div className={styles.headline}>
-        <DetailHeadline headline={`${applicant.name} ${applicant.surname}`}/>
-        <Links links={applicant.links}/>
-      </div>
-      <div className={styles.capabilitiesAndCareersContainer}>
-        <CapabilitiesDetail
-          className={styles.capabilities}
-          title={translations.capabilities}
-          capabilities={applicant.capabilities || []}
-        />
-        <CareersDetail className={styles.careers} careers={applicant.careers || []}/>
-      </div>
-      {
-        applicant.sections?.map(section =>
-          <SectionDetail key={section.displayOrder} title={section.title} text={section.text}/>
-        )
-      }
+    translations
+  }) => (
+  <div className={styles.container}>
+    <div className={styles.headline}>
+      <DetailHeadline headline={`${applicant.name} ${applicant.surname}`}/>
+      <Links links={applicant.links}/>
     </div>
-  );
-};
+    <div className={styles.capabilitiesAndCareersContainer}>
+      <CapabilitiesDetail
+        className={styles.capabilities}
+        title={translations.capabilities}
+        capabilities={applicant.capabilities || []}
+      />
+      <CareersDetail className={styles.careers} careers={applicant.careers || []}/>
+    </div>
+    {
+      applicant.sections?.map(section =>
+        <SectionDetail key={section.displayOrder} title={section.title} text={section.text}/>
+      )
+    }
+  </div>
+);
+
+interface ITranslations {
+  padron: string;
+  capabilities: string;
+}
+
+interface IApplicantDetailProps {
+  applicant: IApplicant;
+  translations: ITranslations;
+}
 
 export { Detail };
