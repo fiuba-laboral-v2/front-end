39b48068e5a9fced56645d253fbd07f8cd3d5003
dylan <dylanalvarez1995@gmail.com>
2020-04-04 00:31:11 -0300

    Add margin to title only when subtitle is present

--- src/components/Title/component.tsx
+++ src/components/Title/component.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from "react";
 import { Subtitle } from "$components/Subtitle";
 import styles from "./styles.module.scss";
 import { ITitleProps } from "./interface";
+import classNames from "classnames";
 
 const Title: FunctionComponent<ITitleProps> = (
   {
@@ -10,7 +11,7 @@ const Title: FunctionComponent<ITitleProps> = (
   }
 ) => (
   <div className={styles.container}>
-    <h1 className={styles.title}>{title}</h1>
+    <h1 className={classNames(styles.title, { [styles.noMargin]: !subtitle })}>{title}</h1>
     <Subtitle>{subtitle}</Subtitle>
   </div>
 );
