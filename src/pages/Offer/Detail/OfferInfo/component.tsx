fd80255874dd26f4ca81c4fe656e606332eecbe1
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-04-11 06:01:51 -0300

    Add OfferCareers component

--- src/pages/Offer/Detail/OfferInfo/component.tsx
+++ src/pages/Offer/Detail/OfferInfo/component.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from "react";
 import classNames from "classnames";
 import { OfferInfoItem } from "../OfferInfoItem";
 import { OfferSalary } from "../OfferSalary";
+import { OfferCareers } from "../OfferCareers";
 
 import { IOffer } from "$interfaces/Offer";
 
@@ -9,13 +10,7 @@ import styles from "./styles.module.scss";
 
 const OfferInfo: FunctionComponent<IOfferInfoProps> = ({ offer, className }) => (
   <div className={classNames(styles.mainContainer, className)}>
-    <OfferInfoItem className={styles.careers} title={"Carreras"}>
-      {
-        offer.careers?.map(career =>
-          <div key={career.code} className={styles.career}>{career.description}</div>
-        )
-      }
-    </OfferInfoItem>
+    <OfferCareers className={styles.careers} offer={offer}/>
     <OfferInfoItem className={styles.hours} title={"Carga Horaria"}>
       <div className={styles.hoursInfo}>
         <div className={styles.hoursNumber}>{offer.hoursPerDay}</div>
