92547a092fc2ed7476c8aed4af83d8bd6e73ab01
Dylan Alvarez <dalvarez@devartis.com>
2020-04-11 19:31:46 -0300

    Simplify Tag component

--- src/components/Tag/component.tsx
+++ src/components/Tag/component.tsx
@@ -1,23 +1,17 @@
-import React, { Fragment, FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 import styles from "./styles.module.scss";
 import classNames from "classnames";
 import { RemoveButton } from "../RemoveButton";
 
-const Tag: FunctionComponent<ITagProps> = ({ name, className, onRemove }) => {
-  let removeButton;
-  if (onRemove) {
-    removeButton = <RemoveButton className={styles.removeButton} onClick={() => onRemove(name)}/>;
-  } else {
-    removeButton = <Fragment/>;
-  }
-
-  return (
-    <div className={classNames(styles.tag, className)}>
-      {name}
-      {removeButton}
-    </div>
-  );
-};
+const Tag: FunctionComponent<ITagProps> = ({ name, className, onRemove }) => (
+  <div className={classNames(styles.tag, className)}>
+    {name}
+    {onRemove && <RemoveButton
+        className={styles.removeButton}
+        onClick={() => onRemove(name)}
+    />}
+  </div>
+);
 
 interface ITagProps {
   name: string;
