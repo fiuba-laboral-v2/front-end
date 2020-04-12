4c62c8728b2ceda2e60becb66a70a737a5c8ed16
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-04-12 04:10:22 -0300

    Move each salary info to its aown component

--- src/pages/Applicant/OfferDetail/OfferSalary/component.tsx
+++ src/pages/Applicant/OfferDetail/OfferSalary/component.tsx
@@ -1,8 +1,8 @@
 import React, { FunctionComponent } from "react";
 import classNames from "classnames";
 import styles from "./styles.module.scss";
-import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
 import { OfferInfoItem } from "../OfferInfoItem";
+import { OfferSalaryInfo } from "../OfferSalaryInfo";
 
 import { IOfferSalaryProps } from "./interface";
 
@@ -13,16 +13,14 @@ const OfferSalary: FunctionComponent<IOfferSalaryProps> = (
     className
   }) => (
     <OfferInfoItem className={classNames(styles.salary, className)} title={translations.title}>
-      <div className={styles.SalaryInfo}>
-        <div className={styles.salaryInfoText}>{translations.salaryFrom}</div>
-        <AttachMoneyIcon fontSize="small"/>
-        <div className={styles.salaryNumber}>{offer.minimumSalary}</div>
-      </div>
-      <div className={styles.SalaryInfo}>
-        <div className={styles.salaryInfoText}>{translations.salaryTo}</div>
-        <AttachMoneyIcon fontSize="small"/>
-        <div className={styles.salaryNumber}>{offer.maximumSalary}</div>
-      </div>
+      <OfferSalaryInfo
+        translations={{ salaryInfoText: translations.salaryFrom }}
+        salaryNumber={offer.minimumSalary}
+      />
+      <OfferSalaryInfo
+        translations={{ salaryInfoText: translations.salaryTo }}
+        salaryNumber={offer.maximumSalary}
+      />
     </OfferInfoItem>
 );
 
---
+++ src/pages/Applicant/OfferDetail/OfferSalaryInfo/component.tsx
@@ -0,0 +1,26 @@
+import React, { FunctionComponent } from "react";
+import styles from "./styles.module.scss";
+import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
+
+const OfferSalaryInfo: FunctionComponent<IOfferSalaryInfoProps> = (
+  {
+    salaryNumber,
+    translations
+  }) => (
+  <div className={styles.salaryInfo}>
+    <span className={styles.salaryInfoText}>{translations.salaryInfoText}</span>
+    <AttachMoneyIcon fontSize="small"/>
+    <span className={styles.salaryNumber}>{salaryNumber}</span>
+  </div>
+);
+
+interface IOfferSalaryInfoTranslations {
+  salaryInfoText: string;
+}
+
+interface IOfferSalaryInfoProps {
+  translations: IOfferSalaryInfoTranslations;
+  salaryNumber: number;
+}
+
+export { OfferSalaryInfo };
