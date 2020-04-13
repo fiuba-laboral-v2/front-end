2fdaff2072c29ba4b4a07855b19f53897834cfe0
llmanuel <llauromanuel@gmail.com>
2020-04-13 01:20:38 -0300

    Remove mocks. Add query

--- src/pages/Applicant/Home/Feed/container.tsx
+++ src/pages/Applicant/Home/Feed/container.tsx
@@ -1,10 +1,23 @@
 import React, { FunctionComponent } from "react";
 import { Feed } from "./component";
-
-import getOffers from "./constants";
+import { GET_OFFERS } from "$queries";
+import { IOffer } from "$interfaces/Offer";
+import { useQuery } from "@apollo/react-hooks";
+import { LoadingSpinner } from "$components/LoadingSpinner";
+import { RoutesBuilder } from "$models/RoutesBuilder";
+import { useHistory } from "react-router-dom";
 
 const FeedContainer: FunctionComponent = () => {
-  const offers = getOffers();
+  const history = useHistory();
+
+  const {
+    data: { getOffers: offers } = { getOffers: {} as IOffer[] },
+    error: offerError,
+    loading: loadingOffer
+  } = useQuery(GET_OFFERS);
+
+  if (offerError) history.push(RoutesBuilder.notFound);
+  if (loadingOffer) return <LoadingSpinner/>;
 
   return (
     <Feed offers={offers} />
