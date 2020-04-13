685623c88a1092c95057ed182acb7037f7d02536
Dylan Alvarez <dalvarez@devartis.com>
2020-04-12 22:11:30 -0300

    Fix company name spacing

--- src/pages/Applicant/Home/Feed/Offer/Info/JobSpecs/index.tsx
+++ src/pages/Applicant/Home/Feed/Offer/Info/JobSpecs/index.tsx
@@ -3,19 +3,20 @@ import AttachMoneyIcon from "@material-ui/icons/AttachMoney";
 import AccessTimeIcon from "@material-ui/icons/AccessTime";
 import { SpecItem } from "./SpecItem";
 
-const JobSpecs: FunctionComponent<IJobSpecsProps> = ({salary, workload}) => (
-  <div>
+const JobSpecs: FunctionComponent<IJobSpecsProps> = ({ salary, workload, className }) => (
+  <div className={className}>
     <SpecItem item={workload.time} description={workload.description}>
-      <AccessTimeIcon fontSize="small" />
+      <AccessTimeIcon fontSize="small"/>
     </SpecItem>
-    <SpecItem item={salary} >
-    <AttachMoneyIcon fontSize="small"/>
+    <SpecItem item={salary}>
+      <AttachMoneyIcon fontSize="small"/>
     </SpecItem>
   </div>
 );
 
 interface IJobSpecsProps {
   salary: string;
+  className: string;
   workload: {
     time: string;
     description: string;
