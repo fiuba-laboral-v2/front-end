6cd2dc0206fb8dc9598cd3496f58675c25951c9b
Dylan Alvarez <dalvarez@devartis.com>
2020-04-12 23:40:09 -0300

    Animate card on mobile

--- src/components/ListItem/component.tsx
+++ src/components/ListItem/component.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import classnames from "classnames";
 
 import styles from "./styles.module.scss";
@@ -9,14 +9,23 @@ const ListItem: FunctionComponent<IListItemProps> = (
     className,
     onClick
   }
-) => (
-  <div
-    className={classnames(styles.listItem, className, { [styles.hoverable]: onClick })}
-    onClick={onClick}
-  >
-    {children}
-  </div>
-);
+) => {
+  const [touched, setTouched] = useState(false);
+  return (
+    <div
+      className={classnames(styles.listItem, className, {
+        [styles.hoverable]: onClick,
+        [styles.touched]: touched
+      })}
+      onClick={onClick}
+      onTouchStart={() => setTouched(true)}
+      onTouchMove={() => setTouched(false)}
+      onTouchEnd={() => setTouched(false)}
+    >
+      {children}
+    </div>
+  );
+};
 
 interface IListItemProps {
   className?: string;
