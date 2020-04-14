36ba2a747521fc0d4c3826c7522dda44a748d5f0
Dylan Alvarez <dalvarez@devartis.com>
2020-04-14 19:45:23 -0300

    Fix 404 page styles

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,14 +1,22 @@
 import React, { FunctionComponent } from "react";
 
 import styles from "./styles.module.scss";
+import { Title } from "$components/Title";
+import { Window } from "$components/Window";
+import Button from "$components/Button";
+import { Link } from "react-router-dom";
+import { RoutesBuilder } from "$models/RoutesBuilder";
 
 const NotFoundPage: FunctionComponent = () => (
-  <div>
-    <img src={"images/notFound.jpg"} alt="notFound" className={styles.notFound}/>
-    <div>
-      <h1 className={styles.notFoundText}>404</h1>
+  <Window>
+    <div className={styles.container}>
+      <Title title={"Parece que esta página no existe"}/>
+      <img src={"images/brokenLink.svg"} alt="Not found" className={styles.image}/>
+      <Link to={RoutesBuilder.applicant.home()} className={styles.link}>
+        <Button className={"primary"}>{"Ir a la página principal"}</Button>
+      </Link>
     </div>
-  </div>
+  </Window>
 );
 
 export default NotFoundPage;
