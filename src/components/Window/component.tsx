c33fab33908a3cb52ae88a65061d874bfe92376e
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-04-17 21:05:39 -0300

    Rollback style in window

--- src/components/Window/component.tsx
+++ src/components/Window/component.tsx
@@ -3,10 +3,8 @@ import React, { FunctionComponent } from "react";
 import { NavBar } from "../NavBar";
 import { MainContent } from "../MainContent";
 
-import styles from "./styles.module.scss";
-
 export const Window: FunctionComponent = ({ children }) => (
-  <div className={styles.window} >
+  <div>
     <NavBar/>
     <MainContent>
       {children}
