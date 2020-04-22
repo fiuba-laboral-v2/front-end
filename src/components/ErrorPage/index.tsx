522dda37048e626d39f6ed7c6032a48e4e9bc362
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-04-21 22:17:52 -0300

    Remove NavBar from error pages

--- src/components/ErrorPage/index.tsx
+++ src/components/ErrorPage/index.tsx
@@ -1,7 +1,6 @@
 import React, { FunctionComponent } from "react";
 import { Title } from "$components/Title";
 import Button from "$components/Button";
-import { Window } from "$components/Window";
 import styles from "./styles.module.scss";
 
 export const ErrorPage: FunctionComponent<IErrorPageProps> = (
@@ -13,13 +12,11 @@ export const ErrorPage: FunctionComponent<IErrorPageProps> = (
     goTo
   }
 ) => (
-  <Window>
-    <section className={styles.error}>
-      <Title title={title}/>
-      <img className={styles.imgSrc} src={imgSrc} alt={errorType}/>
-      <Button onClick={goTo} className="primary">{buttonMessage}</Button>
-    </section>
-  </Window>
+  <section className={styles.error}>
+    <Title title={title}/>
+    <img className={styles.imgSrc} src={imgSrc} alt={errorType}/>
+    <Button onClick={goTo} className="primary">{buttonMessage}</Button>
+  </section>
 );
 
 interface IErrorPageProps {
