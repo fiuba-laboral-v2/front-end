1123e2666df80c305f1386334ae924354fe79902
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-04-29 08:53:38 -0300

    Fix handleError test by putting the defined errors

--- src/test/models/handleError.test.ts
+++ src/test/models/handleError.test.ts
@@ -24,35 +24,35 @@ describe("handleError", () => {
   });
 
   it("should execute the BadCredentials error and not default", () => {
-    const error = createError("BadCredentials");
-    const defaultCallback = jest.fn(() => undefined);
-    const badCredentialsCallback = jest.fn(() => undefined);
+    const error = createError("BadCredentialsError");
+    const defaultErrorCallback = jest.fn(() => undefined);
+    const badCredentialsErrorCallback = jest.fn(() => undefined);
     handleError(
       error,
       {
-        BadCredentials: badCredentialsCallback,
-        DefaultError: defaultCallback
+        BadCredentialsError: badCredentialsErrorCallback,
+        DefaultError: defaultErrorCallback
       }
       );
-    expect(badCredentialsCallback.mock.calls.length).toBe(1);
-    expect(defaultCallback.mock.calls.length).toBe(0);
+    expect(badCredentialsErrorCallback.mock.calls.length).toBe(1);
+    expect(defaultErrorCallback.mock.calls.length).toBe(0);
   });
 
   it("should only execute the UserEmailAlreadyExistsError", () => {
     const error = createError("UserEmailAlreadyExistsError");
-    const defaultCallback = jest.fn(() => undefined);
-    const badCredentialsCallback = jest.fn(() => undefined);
+    const defaultErrorCallback = jest.fn(() => undefined);
+    const badCredentialsErrorCallback = jest.fn(() => undefined);
     const userEmailAlreadyExistsErrorCallback = jest.fn(() => undefined);
     handleError(
       error,
       {
         UserEmailAlreadyExistsError: userEmailAlreadyExistsErrorCallback,
-        BadCredentials: badCredentialsCallback,
-        DefaultError: defaultCallback
+        BadCredentialsError: badCredentialsErrorCallback,
+        DefaultError: defaultErrorCallback
       }
     );
     expect(userEmailAlreadyExistsErrorCallback.mock.calls.length).toBe(1);
-    expect(badCredentialsCallback.mock.calls.length).toBe(0);
-    expect(defaultCallback.mock.calls.length).toBe(0);
+    expect(badCredentialsErrorCallback.mock.calls.length).toBe(0);
+    expect(defaultErrorCallback.mock.calls.length).toBe(0);
   });
 });
