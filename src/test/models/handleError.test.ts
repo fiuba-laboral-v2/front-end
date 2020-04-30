77863ccd33d7e2b74174c296540090c942b11054
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-04-29 23:49:01 -0300

    Test having more than one graphQL error

--- src/test/models/handleError.test.ts
+++ src/test/models/handleError.test.ts
@@ -2,18 +2,14 @@ import { ApolloError } from "apollo-client";
 import { GraphQLError } from "graphql";
 import { handleError } from "$models/handleError";
 
+interface ICreateGraphQLErrorProps {
+  errorType?: string;
+  withNoData?: boolean;
+}
+
 describe("handleError", () => {
-  const createError = (
-    {
-      errorType,
-      withNoData = false
-    }:
-    {
-      errorType?: string,
-      withNoData?: boolean
-    }
-  ) => {
-    const error = new GraphQLError(
+  const createGraphQLError = ({ errorType, withNoData = false }: ICreateGraphQLErrorProps) => {
+    return new GraphQLError(
       "some message",
       undefined,
       undefined,
@@ -22,28 +18,32 @@ describe("handleError", () => {
       undefined,
       withNoData ? undefined : { data: { errorType: errorType } }
     );
-    return new ApolloError({ graphQLErrors: [error] });
+  };
+
+  const createError = (errorTypes: ICreateGraphQLErrorProps[]) => {
+    const errors = errorTypes.map(errorType => createGraphQLError(errorType));
+    return new ApolloError({ graphQLErrors: errors });
   };
 
   it("should do nothing if no callback is given", () => {
-    const error = createError({ errorType: "customError" });
+    const error = createError([{ errorType: "customError" }]);
     handleError(error, {});
   });
 
   it("should do nothing if there is no data in the error", () => {
-    const error = createError({ withNoData: true });
+    const error = createError([{ withNoData: true }]);
     handleError(error, {});
   });
 
   it("should execute the default error", () => {
-    const error = createError({ errorType: "customError" });
+    const error = createError([{ errorType: "customError" }]);
     const defaultCallback = jest.fn(() => undefined);
     handleError(error, { DefaultError: defaultCallback });
     expect(defaultCallback.mock.calls.length).toBe(1);
   });
 
   it("should execute the BadCredentials error and not default", () => {
-    const error = createError({ errorType: "BadCredentialsError" });
+    const error = createError([{ errorType: "BadCredentialsError" }]);
     const defaultErrorCallback = jest.fn(() => undefined);
     const badCredentialsErrorCallback = jest.fn(() => undefined);
     handleError(
@@ -58,7 +58,7 @@ describe("handleError", () => {
   });
 
   it("should only execute the UserEmailAlreadyExistsError", () => {
-    const error = createError({ errorType: "UserEmailAlreadyExistsError" });
+    const error = createError([{ errorType: "UserEmailAlreadyExistsError" }]);
     const defaultErrorCallback = jest.fn(() => undefined);
     const badCredentialsErrorCallback = jest.fn(() => undefined);
     const userEmailAlreadyExistsErrorCallback = jest.fn(() => undefined);
@@ -73,5 +73,25 @@ describe("handleError", () => {
     expect(userEmailAlreadyExistsErrorCallback.mock.calls.length).toBe(1);
     expect(badCredentialsErrorCallback.mock.calls.length).toBe(0);
     expect(defaultErrorCallback.mock.calls.length).toBe(0);
+  });
+
+  it("should execute the UserEmailAlreadyExistsError and BadCredentialsError", () => {
+    const error = createError(
+      [
+        { errorType: "UserEmailAlreadyExistsError" },
+        { errorType: "BadCredentialsError" }
+      ]
+    );
+    const badCredentialsErrorCallback = jest.fn(() => undefined);
+    const userEmailAlreadyExistsErrorCallback = jest.fn(() => undefined);
+    handleError(
+      error,
+      {
+        UserEmailAlreadyExistsError: userEmailAlreadyExistsErrorCallback,
+        BadCredentialsError: badCredentialsErrorCallback
+      }
+    );
+    expect(userEmailAlreadyExistsErrorCallback.mock.calls.length).toBe(1);
+    expect(badCredentialsErrorCallback.mock.calls.length).toBe(1);
   });
 });
