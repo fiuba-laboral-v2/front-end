7ec34d8853e1a6d5241c845e737a4fcc04204dd7
Dylan Alvarez <dalvarez@devartis.com>
2020-05-12 21:53:21 -0300

    Add mandatory fields to create offer form

--- src/pages/Company/CreateOffer/container.tsx
+++ src/pages/Company/CreateOffer/container.tsx
@@ -2,7 +2,18 @@ import React, { FunctionComponent } from "react";
 import { CreateOffer } from "./component";
 
 export const CreateOfferContainer: FunctionComponent = () => {
-  return <CreateOffer onSubmit={() => {
-    alert("submit!");
-  }}/>;
+  return <CreateOffer
+    onSubmit={() => {
+      alert("submit!");
+    }}
+    translations={{
+      formTitle: "formTitle",
+      offerTitle: "offerTitle",
+      description: "description",
+      hoursPerDay: "hoursPerDay",
+      minimumSalary: "minimumSalary",
+      maximumSalary: "maximumSalary",
+      submit: "submit"
+    }}
+  />;
 };
