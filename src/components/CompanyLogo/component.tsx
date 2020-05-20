ca321329c8838edc97803ee23bcb427937297503
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-05-19 23:48:17 -0300

    Rollback alt display

--- src/components/CompanyLogo/component.tsx
+++ src/components/CompanyLogo/component.tsx
@@ -17,7 +17,7 @@ const CompanyLogo: FunctionComponent<ICompanyLogoProps> = (
     <img
       className={styles.logo}
       src={logo}
-      alt={companyName || "Logo de la empresa"}
+      alt={`Logo de ${companyName || "la empresa"}`}
     />
     {children}
   </div>
