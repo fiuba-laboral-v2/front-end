0576144e67a70f268ea951d8f176d01df0d132b6
Dylan Alvarez <dalvarez@devartis.com>
2020-05-25 20:42:15 -0300

    Move networking logic to container

--- src/pages/Applicant/Profile/component.tsx
+++ src/pages/Applicant/Profile/component.tsx
@@ -1,41 +1,24 @@
-import React, { Fragment, FunctionComponent } from "react";
+import React, { FunctionComponent } from "react";
 import { Title } from "./Title";
 import { ApplicantDetail } from "$components/ApplicantDetail";
 import { Window } from "$components/Window";
-import { IApplicant } from "$interfaces/Applicant";
-import { useHistory } from "react-router-dom";
-import { RoutesBuilder } from "$models/RoutesBuilder";
 import Button from "$components/Button";
-import { useTranslations } from "../../../models/hooks/queries";
-import { Redirect } from "../../../components/Redirect";
+import { IProfileParams } from "./interface";
 
-export const Profile: FunctionComponent<IProfile> = ({ applicant }) => {
-  const history = useHistory();
-  const translations = useTranslations<ITranslations>("applicantProfileDetail");
-  if (translations.loading) return <Fragment/>;
-  if (translations.error) return <Redirect to={RoutesBuilder.public.internalServerError}/>;
-
-  return (
-    <Window>
-      <Title/>
-      <ApplicantDetail
-        applicant={applicant}
-        editButton={
-          <Button
-            className={"primary"}
-            onClick={() => history.push(RoutesBuilder.applicant.editMyProfile)}>
-            {translations.data.edit}
-          </Button>
-        }
-      />
-    </Window>
-  );
-};
-
-interface IProfile {
-  applicant: IApplicant;
-}
-
-interface ITranslations {
-  edit: string;
-}
+export const Profile: FunctionComponent<IProfileParams> = (
+  {
+    applicant,
+    translations,
+    onClickEdit
+  }
+) => (
+  <Window>
+    <Title/>
+    <ApplicantDetail
+      applicant={applicant}
+      editButton={
+        <Button className={"primary"} onClick={onClickEdit}>{translations.edit}</Button>
+      }
+    />
+  </Window>
+);
--- src/pages/Applicant/Profile/container.tsx
+++ src/pages/Applicant/Profile/container.tsx
@@ -1,16 +1,26 @@
-import React, { FunctionComponent } from "react";
+import React, { Fragment, FunctionComponent } from "react";
 import { RoutesBuilder } from "$models/RoutesBuilder";
 
 import { LoadingSpinner } from "$components/LoadingSpinner";
 import { Redirect } from "$components/Redirect";
 import { Profile } from "./component";
-import { useMyApplicantProfile } from "$hooks";
+import { useMyApplicantProfile, useTranslations } from "$hooks";
+import { useHistory } from "react-router-dom";
+import { ITranslations } from "./interface";
 
 export const ProfileContainer: FunctionComponent = () => {
   const response = useMyApplicantProfile();
+  const history = useHistory();
+  const translations = useTranslations<ITranslations>("applicantProfileDetail");
+  if (translations.loading) return <Fragment/>;
+  if (translations.error) return <Redirect to={RoutesBuilder.public.internalServerError}/>;
 
   if (response.error) return <Redirect to={RoutesBuilder.public.internalServerError}/>;
   if (response.loading) return <LoadingSpinner/>;
 
-  return <Profile applicant={response.data.getCurrentUser.applicant}/>;
+  return <Profile
+    applicant={response.data.getCurrentUser.applicant}
+    translations={translations.data}
+    onClickEdit={() => history.push(RoutesBuilder.applicant.editMyProfile)}
+  />;
 };
