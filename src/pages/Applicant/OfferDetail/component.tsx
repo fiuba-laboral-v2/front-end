55eed1df9d61a31197ee19416dad398a7e29c645
Dylan Alvarez <dalvarez@devartis.com>
2020-05-26 16:18:30 -0300

    Fix spacing in offer detail

--- src/pages/Applicant/OfferDetail/component.tsx
+++ src/pages/Applicant/OfferDetail/component.tsx
@@ -2,10 +2,10 @@ import React, { FunctionComponent } from "react";
 import { OfferDetail as Detail } from "$components/OfferDetail";
 import Button from "$components/Button";
 import { Window } from "$components/Window";
-
 import { IOfferDetailTranslations } from "./interfaces";
 import { IMyOffer } from "$interfaces/Applicant";
 import { RoutesBuilder } from "$models/RoutesBuilder";
+import styles from "./styles.module.scss";
 
 export const OfferDetail: FunctionComponent<IOfferDetail> = (
   {
@@ -20,16 +20,18 @@ export const OfferDetail: FunctionComponent<IOfferDetail> = (
         goToCompany={RoutesBuilder.applicant.companyProfile(offer.company.uuid)}
         offer={offer}
         applyButton={
-          <Button
-            onClick={() => apply(offer.uuid)}
-            className="primary"
-            width="expand"
-            type="submit"
-            disabled={offer.hasApplied}
-            title={offer.hasApplied ? translations.alreadyApplied : ""}
-          >
-            {translations.apply}
-          </Button>
+          <div className={styles.applyButton}>
+            <Button
+              onClick={() => apply(offer.uuid)}
+              className="primary"
+              width="expand"
+              type="submit"
+              disabled={offer.hasApplied}
+              title={offer.hasApplied ? translations.alreadyApplied : ""}
+            >
+              {translations.apply}
+            </Button>
+          </div>
         }
       />
     </Window>
