54832642a8a0f2796acbd31ff484b58dbe376a04
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-05-26 16:28:08 -0300

    Remove unnecesary return

--- src/components/CompanyFields/component.tsx
+++ src/components/CompanyFields/component.tsx
@@ -15,37 +15,35 @@ export const CompanyFields: FunctionComponent<ICompanyFieldsProps> = (
   {
     translations
   }
-) => {
-  return (
-    <>
-      <TextInput
-        name="companyName"
-        label={translations.companyName}
-        validate={FormikValidator({ validator: validateName, mandatory: true })}
-      />
-      <TextInput
-        name="cuit"
-        label={translations.cuit}
-        validate={FormikValidator({ validator: validateCuit, mandatory: true })}
-      />
-      <TextInput
-        name="email"
-        label={translations.email}
-        validate={FormikValidator({ validator: validateEmail, mandatory: true })}
-      />
-      <TextInput
-        name="slogan"
-        label={translations.slogan}
-      />
-      <TextInput
-        name="description"
-        label={translations.description}
-      />
-      <TextInput
-        name="website"
-        label={translations.website}
-        validate={FormikValidator({ validator: validateURL, mandatory: true })}
-      />
-    </>
-  );
-};
+) => (
+  <>
+    <TextInput
+      name="companyName"
+      label={translations.companyName}
+      validate={FormikValidator({ validator: validateName, mandatory: true })}
+    />
+    <TextInput
+      name="cuit"
+      label={translations.cuit}
+      validate={FormikValidator({ validator: validateCuit, mandatory: true })}
+    />
+    <TextInput
+      name="email"
+      label={translations.email}
+      validate={FormikValidator({ validator: validateEmail, mandatory: true })}
+    />
+    <TextInput
+      name="slogan"
+      label={translations.slogan}
+    />
+    <TextInput
+      name="description"
+      label={translations.description}
+    />
+    <TextInput
+      name="website"
+      label={translations.website}
+      validate={FormikValidator({ validator: validateURL, mandatory: true })}
+    />
+  </>
+);
