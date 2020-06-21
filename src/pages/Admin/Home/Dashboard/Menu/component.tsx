80d7d646f73f00aca578d2f136b38f1696e50cbb
Dylan Alvarez <dalvarez@devartis.com>
2020-06-21 19:59:23 -0300

    Adapt icon column width by screen width instead of by hover

--- src/pages/Admin/Home/Dashboard/Menu/component.tsx
+++ src/pages/Admin/Home/Dashboard/Menu/component.tsx
@@ -2,12 +2,19 @@ import React, { FunctionComponent } from "react";
 import BusinessIcon from "@material-ui/icons/Business";
 
 import styles from "./styles.module.scss";
+import Tooltip from "@material-ui/core/Tooltip";
+
+const companyIconTitle = "Registro de empresa pendiente";
 
 const Menu: FunctionComponent = () => (
   <div className={styles.menuContent}>
     <div className={styles.company}>
-      <BusinessIcon className={styles.companyIcon} fontSize="default"/>
-      <p className={styles.companyDescription}>Registro de empresa pendiente</p>
+      <Tooltip classes={{ tooltip: styles.tooltip }}
+               title={companyIconTitle}
+               placement="right">
+        <BusinessIcon className={styles.companyIcon} fontSize="default"/>
+      </Tooltip>
+      <p className={styles.companyDescription}>{companyIconTitle}</p>
     </div>
   </div>
 );
