3b391a1e34ba0cc0f1b24889698a584f12a5cb4f
Dylan Alvarez <dalvarez@devartis.com>
2020-06-22 02:35:09 -0300

    Use List component in ListBody

--- src/pages/Admin/Home/Dashboard/AdminTaskList/ListBody/component.tsx
+++ src/pages/Admin/Home/Dashboard/AdminTaskList/ListBody/component.tsx
@@ -2,15 +2,18 @@ import React, { FunctionComponent } from "react";
 import { IApprovable } from "$interfaces/Approvable";
 import { ClickableCard } from "$components/ClickableCard";
 import { ApprovableEntity } from "../ApprovableEntity";
+import { List } from "$components/List";
 import styles from "./styles.module.scss";
 
-export const ListBody: FunctionComponent<IListProps> = ({ approvableEntities }) => <>
-  {approvableEntities.map(entity =>
-    <ClickableCard key={entity.uuid} className={styles.card}>
-      <ApprovableEntity approvableEntity={entity}/>
-    </ClickableCard>
-  )}
-</>;
+export const ListBody: FunctionComponent<IListProps> = ({ approvableEntities }) => (
+  <List list={approvableEntities}>
+    {entity =>
+      <ClickableCard key={entity.uuid} className={styles.card}>
+        <ApprovableEntity approvableEntity={entity}/>
+      </ClickableCard>
+    }
+  </List>
+);
 
 interface IListProps {
   approvableEntities: IApprovable[];
