9897aaf3efcd8bc6f099b1c851d0cbc428847d43
Dylan Alvarez <dalvarez@devartis.com>
2020-06-23 20:27:32 -0300

    Use ifs instead of a map to determine component to enable correct type inference

--- src/pages/Admin/Home/Dashboard/AdminTaskList/ApprovableEntity/component.tsx
+++ src/pages/Admin/Home/Dashboard/AdminTaskList/ApprovableEntity/component.tsx
@@ -2,13 +2,14 @@ import React, { FunctionComponent } from "react";
 import { IApprovable } from "$interfaces/Approvable";
 import { ApprovableCompany } from "../ApprovableCompany";
 
-const componentsByType = {
-  Company: ApprovableCompany
-};
-
-export const ApprovableEntity: FunctionComponent<IApprovableEntityProps> = props => {
-  const Component = componentsByType[props.approvableEntity.__typename];
-  return <Component {...props}/>;
+export const ApprovableEntity: FunctionComponent<IApprovableEntityProps> = (
+  { approvableEntity }
+) => {
+  if (approvableEntity.__typename === "Company") {
+    return <ApprovableCompany approvableEntity={approvableEntity}/>;
+  } else {
+    return <></>;
+  }
 };
 
 interface IApprovableEntityProps {
