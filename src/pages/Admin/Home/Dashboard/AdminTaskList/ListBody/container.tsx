3465e865f48c1b4036bae75a0021be6873df2646
Dylan Alvarez <dalvarez@devartis.com>
2020-06-23 19:35:01 -0300

    Add state in Admin dashboard for selected task

--- src/pages/Admin/Home/Dashboard/AdminTaskList/ListBody/container.tsx
+++ src/pages/Admin/Home/Dashboard/AdminTaskList/ListBody/container.tsx
@@ -3,12 +3,22 @@ import { ListBody } from "./component";
 import { usePendingEntities } from "$hooks";
 import { Redirect } from "$components/Redirect";
 import { RoutesBuilder } from "$models/RoutesBuilder";
+import { IApprovable } from "$interfaces/Approvable";
 
-export const ListBodyContainer: FunctionComponent = () => {
+export const ListBodyContainer: FunctionComponent<IListBodyContainerProps> = (
+  { onSelectTask }
+) => {
   const response = usePendingEntities();
 
   if (response.loading) return <Fragment/>;
   if (response.error) return <Redirect to={RoutesBuilder.public.internalServerError()}/>;
 
-  return <ListBody approvableEntities={response.data.getPendingEntities}/>;
+  return <ListBody
+    approvableEntities={response.data.getPendingEntities}
+    onSelectTask={onSelectTask}
+  />;
 };
+
+interface IListBodyContainerProps {
+  onSelectTask: (task: IApprovable) => void;
+}
--- src/pages/Admin/Home/Dashboard/component.tsx
+++ src/pages/Admin/Home/Dashboard/component.tsx
@@ -1,17 +1,19 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 import { Window } from "$components/Window";
 import { Menu } from "./Menu";
 import { AdminTaskDetail } from "./AdminTaskDetail";
 import { AdminTaskList } from "./AdminTaskList";
-
 import styles from "./styles.module.scss";
+import { IApprovable } from "$interfaces/Approvable";
 
 const Dashboard: FunctionComponent = () => {
+  const [selectedTask, setSelectedTask] = useState<IApprovable>();
+
   return (
     <Window width="fullWidth">
       <div className={styles.mainContent}>
         <Menu/>
-        <AdminTaskList/>
+        <AdminTaskList onSelectTask={setSelectedTask}/>
         <AdminTaskDetail/>
       </div>
     </Window>
