443e32267d8c488dbeaaf6777ff1f82595dbeacb
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-06-26 01:39:40 -0300

    Return only EmptyDetailContent component in AdminTaskDetail if there are no selected tasks

--- src/pages/Admin/Home/Dashboard/AdminTaskDetail/component.tsx
+++ src/pages/Admin/Home/Dashboard/AdminTaskDetail/component.tsx
@@ -10,26 +10,26 @@ export const AdminTaskDetail: FunctionComponent<IAdminTaskDetailProps> = (
   {
     selectedTask
   }
-) => (
-  <>
-    <div className={styles.info}>
-      {
-        selectedTask?.__typename === "Company" &&
-        <CompanyDetailInfo selectedCompany={selectedTask}/>
-      }
-    </div>
-    <div className={styles.content}>
-      {
-        !selectedTask &&
-        <EmptyDetailContent/>
-      }
-      {
-        selectedTask?.__typename === "Company" &&
-        <CompanyDetailContent selectedCompany={selectedTask}/>
-      }
-    </div>
-  </>
-);
+) => {
+  if (!selectedTask) return <EmptyDetailContent/>;
+
+  return (
+    <>
+      <div className={styles.info}>
+        {
+          selectedTask.__typename === "Company" &&
+          <CompanyDetailInfo selectedCompany={selectedTask}/>
+        }
+      </div>
+      <div className={styles.content}>
+        {
+          selectedTask.__typename === "Company" &&
+          <CompanyDetailContent selectedCompany={selectedTask}/>
+        }
+      </div>
+    </>
+  );
+};
 
 interface IAdminTaskDetailProps {
   selectedTask?: IApprovable;
