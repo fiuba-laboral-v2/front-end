3afae7229714e404fdb3336025b4105e47abf239
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-06-30 01:51:12 -0300

    Add shecma generate configuration

--- src/config/Configuration.ts
+++ src/config/Configuration.ts
@@ -1,6 +1,26 @@
-import configurationVariables from "$config/configurationVariables";
+const configurationForAllEnvironments = {
+  production: {
+    sub_domain: "/",
+    application_base_url: "http://laboral.fi.uba.ar/graphql"
+  },
+  staging: {
+    sub_domain: "/",
+    application_base_url: "http://antiguos.fi.uba.ar/graphql"
+  },
+  development: {
+    sub_domain: "/",
+    application_base_url: "http://localhost:5006/graphql"
+  },
+  test: {
+    sub_domain: "/",
+    application_base_url: "http://localhost:5006/graphql"
+  },
+  test_travis: {
+    sub_domain: "/",
+    application_base_url: "http://localhost:5006/graphql"
+  }
+};
 
-const env = (process.env.REACT_APP_STAGE || process.env.NODE_ENV) as ("production" | "staging" | "development" | "test" | "test_travis");
-const Configuration = configurationVariables[env];
-
-export default Configuration;
+type Env = "production" | "staging" | "development" | "test" | "test_travis";
+const env = (process.env.REACT_APP_STAGE || process.env.NODE_ENV) as Env;
+export const Configuration = configurationForAllEnvironments[env];
