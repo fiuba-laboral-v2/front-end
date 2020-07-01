6ee788c3e60aa22a23448266886c989fd104c66c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-07-01 03:33:37 -0300

    Add Applicant icon to the menu tab in the admin homepage

--- src/pages/Admin/Home/Dashboard/Menu/component.tsx
+++ src/pages/Admin/Home/Dashboard/Menu/component.tsx
@@ -1,21 +1,14 @@
 import React, { FunctionComponent } from "react";
-import Tooltip from "@material-ui/core/Tooltip";
 import { IMenuTranslations } from "./interfaces";
+import { Tab } from "./Tab";
 import { CompanyIcon } from "../CompanyIcon";
+import { ApplicantIcon } from "../ApplicantIcon";
 import styles from "./styles.module.scss";
 
 export const Menu: FunctionComponent<IMenu> = ({ translations }) => (
   <div className={styles.menuContent}>
-    <div className={styles.company}>
-      <Tooltip
-        classes={{ tooltip: styles.tooltip }}
-        title={translations.companyIconTitle}
-        placement="right"
-      >
-        <CompanyIcon className={styles.companyIcon}/>
-      </Tooltip>
-      <p className={styles.companyDescription}>{translations.companyIconTitle}</p>
-    </div>
+    <Tab Icon={CompanyIcon} iconTitle={translations.companyIconTitle}/>
+    <Tab Icon={ApplicantIcon} iconTitle={translations.applicantIconTitle}/>
   </div>
 );
 
