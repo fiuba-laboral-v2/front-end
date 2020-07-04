88db01151841d4be27cbb0a1828ac3328f873a9d
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-07-04 00:35:57 -0300

    Refactor ApplicantDetailContentContainer to receive applicantUuid and CompanyDetailContentContainer to receive companyUuid

--- src/pages/Admin/Home/Dashboard/TaskDetail/Applicant/ApplicantDetailContent/container.tsx
+++ src/pages/Admin/Home/Dashboard/TaskDetail/Applicant/ApplicantDetailContent/container.tsx
@@ -2,20 +2,19 @@ import React, { FunctionComponent } from "react";
 import { ApplicantDetail } from "$components/ApplicantDetail";
 import { useApplicantByUuid } from "$hooks/queries";
 import { LoadingSpinner } from "$components/LoadingSpinner";
-import { IApprovableApplicant } from "$interfaces/Approvable";
 
 const ApplicantDetailContentContainer: FunctionComponent<IApplicantDetailContentContainerProps> = (
   {
-    selectedApplicant
+    applicantUuid
   }
 ) => {
-  const response = useApplicantByUuid(selectedApplicant.uuid);
+  const response = useApplicantByUuid(applicantUuid);
   if (response.error || response.loading) return <LoadingSpinner/>;
   return <ApplicantDetail applicant={response.data.getApplicant}/>;
 };
 
 interface IApplicantDetailContentContainerProps {
-  selectedApplicant: IApprovableApplicant;
+  applicantUuid: string;
 }
 
 export { ApplicantDetailContentContainer };
