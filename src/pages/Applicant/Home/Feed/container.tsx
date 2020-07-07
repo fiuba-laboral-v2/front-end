e5647d84876e3fc09fe41504835939c4f6d53b16
Dylan Alvarez <dalvarez@devartis.com>
2020-07-07 12:32:12 -0300

    WIP

--- src/pages/Applicant/Home/Feed/container.tsx
+++ src/pages/Applicant/Home/Feed/container.tsx
@@ -2,12 +2,12 @@ import React, { FunctionComponent } from "react";
 import { Feed } from "./component";
 import { LoadingSpinner } from "$components/LoadingSpinner";
 import { RoutesBuilder } from "$models/RoutesBuilder";
-import { useHistory } from "react-router-dom";
+// import { useHistory } from "react-router-dom";
 import { Redirect } from "$components/Redirect";
 import { useOffers } from "$hooks/queries";
 
 const FeedContainer: FunctionComponent = () => {
-  const history = useHistory();
+  // const history = useHistory();
   const response = useOffers();
 
   if (response.error) return <Redirect to={RoutesBuilder.public.internalServerError()}/>;
@@ -16,7 +16,7 @@ const FeedContainer: FunctionComponent = () => {
   return (
     <Feed
       offers={response.data.getOffers}
-      onCardClick={(uuid: string) => history.push(RoutesBuilder.applicant.offerDetail(uuid))}
+      onCardClick={response.fetchMore}
     />
   );
 };
