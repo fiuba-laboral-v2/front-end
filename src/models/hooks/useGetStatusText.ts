74e88cbdea45b79e6948f9483a379bb9fb022381
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-07-16 19:26:43 -0300

    change yellow color

--- src/models/hooks/useGetStatusText.ts
+++ src/models/hooks/useGetStatusText.ts
@@ -3,7 +3,7 @@ import { useTranslations } from "$hooks";
 export const useGetStatusText = ({ translationGroup, withText }: IUseGetStatusText) => {
   const translations = useTranslations<IStatusTranslations>(translationGroup);
   if (!withText) return;
-  if (translations.loading || translations.error) return "Aprobado";
+  if (translations.loading || translations.error) return "";
   return translations.data.text;
 };
 
