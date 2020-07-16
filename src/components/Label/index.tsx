4188c9d0113e8a932d39b13b2765851138a8eb20
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-07-16 18:11:33 -0300

    Add Label component

---
+++ src/components/Label/index.tsx
@@ -0,0 +1,29 @@
+import React, { FunctionComponent } from "react";
+import classNames from "classnames";
+import styles from "./styles.module.scss";
+
+export const Label: FunctionComponent<ILabelProps> = (
+  {
+    className,
+    Icon,
+    text,
+    color
+  }
+) => (
+  <div className={classNames(styles.tag, styles[color], className)}>
+    <Icon className={styles.icon} fontSize="inherit" />
+    {text && <span className={styles.text}>{text}</span>}
+  </div>
+);
+
+interface ILabelProps {
+  className?: string;
+  Icon: FunctionComponent<IIconProps>;
+  color: "red" | "green" | "blue";
+  text?: string;
+}
+
+interface IIconProps {
+  className?: string;
+  fontSize?: "inherit" | "default" | "small" | "large";
+}
