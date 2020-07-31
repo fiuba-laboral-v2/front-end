7a359b497ce195c51225a334535c6642d1d7ece7
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-07-31 00:07:43 -0300

    Push to internalServerError public page on unknown server error in translations query

--- src/models/hooks/queries/useTranslations.ts
+++ src/models/hooks/queries/useTranslations.ts
@@ -1,7 +1,9 @@
+import { useHistory } from "react-router-dom";
 import { useQuery } from "$hooks";
 import { GET_TRANSLATIONS } from "$queries";
 import { useSnackbar } from "notistack";
 import { handleGenericError } from "$models/errorHandlers/handleGenericError";
+import { RoutesBuilder } from "$models/RoutesBuilder";
 
 interface ITranslation {
   key: string;
@@ -25,13 +27,17 @@ const translationMapper = <T, >({ getTranslations }: ITranslationMapperParams): 
 
 export const useTranslations = <T, >(translationGroup: string) => {
   const { enqueueSnackbar } = useSnackbar();
+  const history = useHistory();
   const { data } = useQuery<{ translationGroup: string }, ITranslationMapperParams>(
     GET_TRANSLATIONS,
     {
       variables: { translationGroup },
       errorHandlers: {
         MissingTranslationError: handleGenericError({ enqueueSnackbar }),
-        defaultHandler: handleGenericError({ enqueueSnackbar })
+        defaultHandler: () => {
+          handleGenericError({ enqueueSnackbar })();
+          history.push(RoutesBuilder.public.internalServerError());
+        }
       }
     }
   );
