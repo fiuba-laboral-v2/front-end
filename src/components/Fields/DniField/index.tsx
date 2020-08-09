ac75c61f6b0923addbd115bc2589485f129fd283
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-08-09 17:25:17 -0300

    Remove validateDni from DniField

--- src/components/Fields/DniField/index.tsx
+++ src/components/Fields/DniField/index.tsx
@@ -1,13 +1,10 @@
 import React, { FunctionComponent } from "react";
 import { TextInput } from "$components/TextInput";
-import { FormikValidator } from "$models/FormikValidator";
 import { IField } from "../interfaces";
-import { validateDni } from "validations-fiuba-laboral-v2";
 
 export const DniField: FunctionComponent<IField> = ({ name, label }) => (
   <TextInput
     name={name}
     label={label}
-    validate={FormikValidator({ validator: validateDni, mandatory: true })}
   />
 );
