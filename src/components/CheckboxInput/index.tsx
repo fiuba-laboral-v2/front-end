8eb2958844fc2bfe42f0de75a6129203b6cfa89c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-08-15 19:25:36 -0300

    Add CheckboxInput component

---
+++ src/components/CheckboxInput/index.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent } from "react";
+import { Field } from "formik";
+import { FieldAttributes } from "formik/dist/Field";
+import { Checkbox } from "formik-material-ui";
+
+export const CheckboxInput: FunctionComponent<FieldAttributes<any>> = props =>
+  <Field
+    size="small"
+    component={Checkbox}
+    {...props}
+  />;
