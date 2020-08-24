f95eaa82998fa0dd6da72560721721603d35b80d
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-08-24 19:39:50 -0300

    Use EmailField and PasswordField in the login component

--- src/components/Fields/EmailField/index.tsx
+++ src/components/Fields/EmailField/index.tsx
@@ -3,11 +3,18 @@ import { TextInput, ITextInputProps } from "$components/TextInput";
 import { FormikValidator } from "$models/FormikValidator";
 import { validateEmail } from "validations-fiuba-laboral-v2";
 
-export const EmailField: FunctionComponent<ITextInputProps> = ({ name, label }) => (
+export const EmailField: FunctionComponent<ITextInputProps> = (
+  {
+    name,
+    label,
+    autoComplete
+  }
+) => (
   <TextInput
     name={name}
     label={label}
     type="email"
+    autoComplete={autoComplete}
     validate={FormikValidator({ validator: validateEmail, mandatory: true })}
   />
 );
