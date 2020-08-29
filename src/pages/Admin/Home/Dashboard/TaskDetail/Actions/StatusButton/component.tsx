de5caf6958cdafec66f21ecf6a7ff60fc07f3a23
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-08-29 19:05:44 -0300

    Remove negative margin

--- src/pages/Admin/Home/Dashboard/TaskDetail/Actions/StatusButton/component.tsx
+++ src/pages/Admin/Home/Dashboard/TaskDetail/Actions/StatusButton/component.tsx
@@ -13,7 +13,9 @@ export const StatusButton: FunctionComponent<IComponent> = (
   }
 ) => (
   <Button className={className} onClick={() => setStatus(status)} >
-    <Icon className={styles.icon} fontSize="small"/>
-    {label}
+    <div className={styles.container}>
+      <Icon className={styles.icon} fontSize="small"/>
+      <span className={styles.label}>{label}</span>
+    </div>
   </Button>
 );
