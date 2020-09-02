c18373add3bd5767ccc3f609a405baffc51cd4d3
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-09-02 01:51:52 -0300

    Run compiler linter before commit

--- src/pages/Admin/Home/Dashboard/TaskDetail/JobApplication/DetailContent/interfaces.ts
+++ src/pages/Admin/Home/Dashboard/TaskDetail/JobApplication/DetailContent/interfaces.ts
@@ -1,6 +1,5 @@
 import { IApplicant } from "$interfaces/Applicant";
 import { IOffer } from "$interfaces/Offer";
-import { ApprovalStatus } from "$interfaces/ApprovalStatus";
 
 export interface IContainerProps {
   applicantUuid: string;
