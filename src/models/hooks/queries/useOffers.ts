5d93f23957f6dad8b413f2ecb0ff11ef9d7b27ac
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-09-06 01:19:43 -0300

    Name parameters from usePaginatedOffers hook

--- src/models/hooks/queries/useOffers.ts
+++ src/models/hooks/queries/useOffers.ts
@@ -1,4 +1,7 @@
 import { GET_OFFERS } from "$queries";
 import { usePaginatedOffers } from "$hooks";
 
-export const useOffers = () => usePaginatedOffers({ documentNode: GET_OFFERS, queryName: "getOffers" });
+export const useOffers = () => usePaginatedOffers({
+  documentNode: GET_OFFERS,
+  queryName: "getOffers"
+});
