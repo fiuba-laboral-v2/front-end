e177dafd3d2b80c0f18dde370af32aad73e4e8e4
Dylan Alvarez <dalvarez@devartis.com>
2020-09-11 04:26:51 -0300

    Make compontents smaller and sort by label

--- src/components/FilterLabels/component.tsx
+++ src/components/FilterLabels/component.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import { FilterLabel } from "$components/FilterLabel";
 import styles from "./styles.module.scss";
 import classNames from "classnames";
+import { sortBy } from "lodash";
 
 export const FilterLabels = <ListItem, >(
   { items, getKey, getLabel, applied, className }: IFilterLabelsProps<ListItem>
 ) => (
   <div className={classNames(styles.container, className)}>
-    {items.map(item =>
+    {sortBy(items, getLabel).map(item =>
       <FilterLabel applied={applied} className={styles.filter} key={getKey(item)}>
         {getLabel(item)}
       </FilterLabel>
