20f25228acbfa132c066f9475790c09ce513d94a
Dylan Alvarez <dalvarez@devartis.com>
2020-09-12 01:09:49 -0300

    Make OfferFilter more clear

--- src/models/OfferFilter.ts
+++ src/models/OfferFilter.ts
@@ -1,5 +1,5 @@
 export class OfferFilter extends URLSearchParams {
   public careerCodes() {
-    return (this.get("carreras")?.split(",") || []).filter(Boolean);
+    return (this.get("carreras")?.split(",") || []).filter(careerCode => careerCode !== "");
   }
 }
