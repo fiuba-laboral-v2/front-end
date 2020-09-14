15da3b353200df3647eb8f293f007c8380951bd0
Dylan Alvarez <dalvarez@devartis.com>
2020-09-14 12:33:34 -0300

    Add mobile styles for offer filter

--- src/pages/Applicant/Home/Feed/Filters/ApplyFilters/component.tsx
+++ src/pages/Applicant/Home/Feed/Filters/ApplyFilters/component.tsx
@@ -1,27 +1,29 @@
 import React, { FunctionComponent } from "react";
-import styles from "./styles.module.scss";
 import { ApplyCareerFilters } from "./ApplyCareerFilters";
 import { ICareer } from "$interfaces/Career";
 import { OfferFilter } from "$models/OfferFilter";
 import { IOfferListTranslations } from "../../interface";
+import { ApplyFiltersTitle } from "../ApplyFiltersTitle";
 
 export const ApplyFilters: FunctionComponent<IApplyFiltersProps> = (
   {
     className,
     filter,
     careers,
-    translations
+    translations,
+    withTitle
   }
 ) => (
   <div className={className}>
-    <h3 className={styles.title}>{translations?.addFilters}</h3>
+    {withTitle && <ApplyFiltersTitle>{translations?.addFilters}</ApplyFiltersTitle>}
     <ApplyCareerFilters filter={filter} careers={careers} translations={translations}/>
   </div>
 );
 
 interface IApplyFiltersProps {
-  className: string;
+  className?: string;
   filter: OfferFilter;
   careers: ICareer[];
   translations?: IOfferListTranslations;
+  withTitle: boolean;
 }
