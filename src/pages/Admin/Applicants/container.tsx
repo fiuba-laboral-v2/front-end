af1561a276e5ef4c5fa81acceea0a596bc3af61b
llmanuel <llauromanuel@gmail.com>
2020-09-17 20:39:28 -0300

    agregado de useCompanies

--- src/pages/Admin/Applicants/container.tsx
+++ src/pages/Admin/Applicants/container.tsx
@@ -2,9 +2,9 @@ import React, { FunctionComponent } from "react";
 import { ListPageContainer } from "../components/ListPageContainer";
 import { ListHeader } from "./ListHeader";
 import { ListContentItem } from "./ListContentItem";
-import styles from "./styles.module.scss";
 import { IApplicant } from "$interfaces/Applicant";
 import { useApplicants } from "$hooks";
+import styles from "./styles.module.scss";
 
 export const Applicants: FunctionComponent<IApplicants> = () => {
   const result = useApplicants();
