0ba0bcc05ce9677d5b1d8a9c6cd7eaa9234d9111
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-04 19:57:03 -0300

    Export inline

--- src/pages/Applicant/EditableProfile/index.tsx
+++ src/pages/Applicant/EditableProfile/index.tsx
@@ -2,10 +2,8 @@ import React, { FunctionComponent } from "react";
 import { EditableDetail } from "./EditableDetail";
 import { Window } from "$components/Window";
 
-const EditableProfile: FunctionComponent = () => (
+export const EditableProfile: FunctionComponent = () => (
   <Window>
     <EditableDetail/>
   </Window>
 );
-
-export { EditableProfile };
