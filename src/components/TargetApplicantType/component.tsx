a09c7d4fbfc892c5bf6f0abb41994361c1a747cd
llmanuel <llauromanuel@gmail.com>
2020-10-06 20:05:11 -0300

    attend comments

--- src/components/TargetApplicantType/component.tsx
+++ src/components/TargetApplicantType/component.tsx
@@ -4,9 +4,5 @@ import { ITargetApplicantType } from "./interfaces";
 export const TargetApplicantType: FunctionComponent<ITargetApplicantType> = (
   { translations, targetApplicantType, className }
 ) => (
-  <>
-    {
-      <span className={className}>{translations[targetApplicantType]}</span>
-    }
-  </>
+  <span className={className}>{translations[targetApplicantType]}</span>
 );
