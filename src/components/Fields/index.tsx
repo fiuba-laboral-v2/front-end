bcf163f061a0a960749704dac84c3aee4620f4e2
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-11 17:43:14 -0300

    Add CuitField component

--- src/components/Fields/index.tsx
+++ src/components/Fields/index.tsx
@@ -2,3 +2,4 @@ export { DniField } from "./DniField";
 export { EmailField } from "./EmailField";
 export { NameField } from "./NameField";
 export { PasswordField } from "./PasswordField";
+export { CuitField } from "./CuitField";
