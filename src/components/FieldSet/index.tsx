e140bda8e1f88497713ea226060b45fabb2fc772
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-11 21:52:27 -0300

    Remove unnecesary component

--- src/components/FieldSet/index.tsx
+++ src/components/FieldSet/index.tsx
@@ -1,24 +1,18 @@
 import React, { FunctionComponent } from "react";
 import styles from "./styles.module.scss";
 import { RemoveButton } from "$components/RemoveButton";
-import { FormSection } from "$components/FormSection";
 
-const FieldSet: FunctionComponent<IFieldSetProps> = (
+export const FieldSet: FunctionComponent<IFieldSetProps> = (
   {
     onRemove,
     children
   }) => (
-  <FormSection>
+  <div className={styles.fieldSetContainer}>
     <div className={styles.childrenContainer}>{children}</div>
-    <RemoveButton
-      className={styles.remove}
-      onClick={onRemove}
-    />
-  </FormSection>
+    <RemoveButton className={styles.remove} onClick={onRemove}/>
+  </div>
 );
 
 export interface IFieldSetProps {
   onRemove: () => void;
 }
-
-export { FieldSet };
