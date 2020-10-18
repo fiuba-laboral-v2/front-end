f1dbdac7d5561eaf904f97915827676e1e33ac13
llmanuel <llauromanuel@gmail.com>
2020-10-18 00:42:03 -0300

    add last empty states

--- src/pages/Applicant/Companies/component.tsx
+++ src/pages/Applicant/Companies/component.tsx
@@ -4,6 +4,7 @@ import { Subtitle } from "$components/Subtitle";
 import { CompanyLogo } from "$components/CompanyLogo";
 import { ListTitle } from "$components/ListTitle";
 import { LoadingSpinner } from "$components/LoadingSpinner";
+import { EmptyList } from "$components/EmptyList";
 import { Window } from "$components/Window";
 import { ICompany } from "$interfaces/Company";
 import styles from "./styles.module.scss";
@@ -25,20 +26,32 @@ export const Companies: FunctionComponent<ICompaniesProps> = ({
   return (
     <Window>
       <ListTitle titleTranslationPath={"companiesList"} />
-      {companies.map(company => (
-        <Card key={company.uuid} className={styles.row} onClick={() => onClickView(company.uuid)}>
-          <div className={styles.childrenContainer}>
-            <CompanyLogo
-              className={styles.companyLogo}
-              size="large"
-              companyName={company.companyName}
-              logo={company.logo}
-              useDefaultIcon
-            />
-            <Subtitle className={styles.name}>{company.companyName}</Subtitle>
-          </div>
-        </Card>
-      ))}
+      {companies.length > 0 &&
+        companies.map(company => (
+          <Card key={company.uuid} className={styles.row} onClick={() => onClickView(company.uuid)}>
+            <div className={styles.childrenContainer}>
+              <CompanyLogo
+                className={styles.companyLogo}
+                size="large"
+                companyName={company.companyName}
+                logo={company.logo}
+                useDefaultIcon
+              />
+              <Subtitle className={styles.name}>{company.companyName}</Subtitle>
+            </div>
+          </Card>
+        ))}
+      {companies.length === 0 && (
+        <div className={styles.emptyListContainer}>
+          <EmptyList
+            emptyTranslationSource="applicantEmptyCompaniesList"
+            buttonKind="secondary"
+            onClick={() => {
+              // sacar los filtros cuando hayan
+            }}
+          />
+        </div>
+      )}
     </Window>
   );
 };
