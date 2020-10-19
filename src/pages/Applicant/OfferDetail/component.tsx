bee5073725ed46a0066224d1613c32376db7d8b5
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-19 18:11:49 -0300

    Remove unnecesary return statement

--- src/pages/Applicant/OfferDetail/component.tsx
+++ src/pages/Applicant/OfferDetail/component.tsx
@@ -7,30 +7,28 @@ import { IMyOffer } from "$interfaces/Applicant";
 import { RoutesBuilder } from "$models/RoutesBuilder";
 import styles from "./styles.module.scss";
 
-export const OfferDetail: FunctionComponent<IOfferDetail> = ({ offer, apply, translations }) => {
-  return (
-    <Window>
-      <Detail
-        goToCompany={RoutesBuilder.applicant.companyProfile(offer.company.uuid)}
-        offer={offer}
-        applyButton={
-          <div className={styles.applyButton}>
-            <Button
-              onClick={() => apply(offer.uuid)}
-              kind="primary"
-              width="expand"
-              type="submit"
-              disabled={offer.hasApplied}
-              title={offer.hasApplied ? translations.alreadyApplied : ""}
-            >
-              {translations.apply}
-            </Button>
-          </div>
-        }
-      />
-    </Window>
-  );
-};
+export const OfferDetail: FunctionComponent<IOfferDetail> = ({ offer, apply, translations }) => (
+  <Window>
+    <Detail
+      goToCompany={RoutesBuilder.applicant.companyProfile(offer.company.uuid)}
+      offer={offer}
+      applyButton={
+        <div className={styles.applyButton}>
+          <Button
+            onClick={() => apply(offer.uuid)}
+            kind="primary"
+            width="expand"
+            type="submit"
+            disabled={offer.hasApplied}
+            title={offer.hasApplied ? translations.alreadyApplied : ""}
+          >
+            {translations.apply}
+          </Button>
+        </div>
+      }
+    />
+  </Window>
+);
 
 interface IOfferDetail {
   offer: IMyOffer;
