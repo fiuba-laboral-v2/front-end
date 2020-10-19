90346a87ef5b25ddd7c5603585abd3fa2a522c47
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-19 18:17:48 -0300

    Remove repeat method call

--- src/pages/Company/EditableProfile/component.tsx
+++ src/pages/Company/EditableProfile/component.tsx
@@ -17,7 +17,7 @@ export const EditableProfile: FunctionComponent<IEditableProfileProps> = ({
   translations,
   acceptanceCriteria
 }) => (
-  <Form title={translations.title} acceptanceCriteria={acceptanceCriteria.repeat(100)}>
+  <Form title={translations.title} acceptanceCriteria={acceptanceCriteria}>
     <Formik initialValues={initialValues} validateOnMount onSubmit={onUpdate}>
       {({ values, setFieldValue, isSubmitting, errors }) => (
         <FormikForm>
