a523ad2a1c0ee4db68d33e3e67d74d0836258e28
Dylan Alvarez <dalvarez@devartis.com>
2020-10-23 20:50:27 -0300

    Remove white screen when loading applicant detail for companies

--- src/pages/Company/Applicant/index.tsx
+++ src/pages/Company/Applicant/index.tsx
@@ -1,1 +1,21 @@
-export { ApplicantContainer as Applicant } from "./container";
+import React, { FunctionComponent } from "react";
+import { useParams } from "react-router-dom";
+import { LoadingSpinner } from "$components/LoadingSpinner";
+import { useApplicantByUuid } from "$hooks";
+import { Window } from "$components/Window";
+import { ApplicantDetail } from "$components/ApplicantDetail";
+
+export const Applicant: FunctionComponent = () => {
+  const { uuid } = useParams();
+  const response = useApplicantByUuid(uuid);
+
+  return (
+    <Window>
+      {response.data?.getApplicant ? (
+        <ApplicantDetail applicant={response.data.getApplicant} />
+      ) : (
+        <LoadingSpinner />
+      )}
+    </Window>
+  );
+};
