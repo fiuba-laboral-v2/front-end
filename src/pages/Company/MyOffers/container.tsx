d2878d26ba69a8f3e91e5e95e9c7a260c5d9a0dc
Dylan Alvarez <dalvarez@devartis.com>
2020-10-23 20:00:23 -0300

    Remove white screen when loading MyOffers translations

--- src/pages/Company/MyOffers/container.tsx
+++ src/pages/Company/MyOffers/container.tsx
@@ -2,12 +2,12 @@ import React, { FunctionComponent } from "react";
 import { useHistory } from "react-router-dom";
 import { useTranslations } from "$hooks";
 import { RoutesBuilder } from "$models/RoutesBuilder";
-import { LoadingSpinner } from "$components/LoadingSpinner";
 import { Redirect } from "$components/Redirect";
 import { Feed } from "$components/Feed";
 import { Window } from "$components/Window";
 import { EmptyList } from "$components/EmptyList";
 import { useMyOffers } from "$hooks/queries/useMyOffers";
+import { LoadingSpinner } from "$components/LoadingSpinner";
 
 export const MyOffersContainer: FunctionComponent = () => {
   const history = useHistory();
@@ -17,26 +17,29 @@ export const MyOffersContainer: FunctionComponent = () => {
   if (response.error) {
     return <Redirect to={RoutesBuilder.public.internalServerError()} />;
   }
-  if (!translations) return <LoadingSpinner />;
 
   return (
     <Window>
-      <Feed
-        loading={response.loading}
-        title={translations.title}
-        offers={response.data?.getMyOffers.results || []}
-        onCardClick={(uuid: string) => history.push(RoutesBuilder.company.offer(uuid))}
-        fetchMore={response.fetchMore}
-        shouldFetchMore={response.data?.getMyOffers.shouldFetchMore}
-        withStatusLabels
-        emptyListComponent={
-          <EmptyList
-            emptyTranslationSource="companyEmptyOfferList"
-            buttonKind="primary"
-            onClick={() => history.push(RoutesBuilder.company.createOffer())}
-          />
-        }
-      />
+      {!translations ? (
+        <LoadingSpinner />
+      ) : (
+        <Feed
+          loading={response.loading}
+          title={translations.title}
+          offers={response.data?.getMyOffers.results || []}
+          onCardClick={(uuid: string) => history.push(RoutesBuilder.company.offer(uuid))}
+          fetchMore={response.fetchMore}
+          shouldFetchMore={response.data?.getMyOffers.shouldFetchMore}
+          withStatusLabels
+          emptyListComponent={
+            <EmptyList
+              emptyTranslationSource="companyEmptyOfferList"
+              buttonKind="primary"
+              onClick={() => history.push(RoutesBuilder.company.createOffer())}
+            />
+          }
+        />
+      )}
     </Window>
   );
 };
