684086e2d954dd2806fb9ea57b1e48bbd8d72a96
Dylan Alvarez <dalvarez@devartis.com>
2020-10-23 21:35:06 -0300

    Remove white screen when loading company detail

--- src/pages/Applicant/Company/index.tsx
+++ src/pages/Applicant/Company/index.tsx
@@ -1,1 +1,25 @@
-export { CompanyProfileContainer as CompanyProfile } from "./container";
+import React, { FunctionComponent } from "react";
+import { useParams } from "react-router-dom";
+import { useCompanyByUuid } from "$hooks";
+import { LoadingSpinner } from "$components/LoadingSpinner";
+import { Window } from "$components/Window";
+import { CompanyDetail } from "$components/CompanyDetail";
+
+export const CompanyProfile: FunctionComponent = () => {
+  const { uuid } = useParams<IRouteParams>();
+  const response = useCompanyByUuid({ uuid });
+
+  return (
+    <Window>
+      {response.data ? (
+        <CompanyDetail company={response.data.getCompanyByUuid} />
+      ) : (
+        <LoadingSpinner />
+      )}
+    </Window>
+  );
+};
+
+interface IRouteParams {
+  uuid: string;
+}
