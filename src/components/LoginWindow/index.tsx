30eafa3bba40342ad97ab4410f42f65fcd18b16b
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-10-25 21:06:35 -0300

    Add LoginWindow component

---
+++ src/components/LoginWindow/index.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from "react";
+import classNames from "classnames";
+
+import styles from "./styles.module.scss";
+
+export const LoginWindow: FunctionComponent<IComponent> = ({ className, children }) => (
+  <div className={classNames(className, styles.mainContent)}>
+    <div className={styles.loginMainContainer}>
+      <div className={styles.logo} />
+      <section className={styles.rightContainer}>{children}</section>
+    </div>
+  </div>
+);
+
+interface IComponent {
+  className?: string;
+}
