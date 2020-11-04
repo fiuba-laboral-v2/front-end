4c529f3e3b67313b4bceaef36b7b8bb1e5a908fe
Dylan Alvarez <dylanalvarez1995@gmail.com>
2020-11-03 23:24:37 -0300

    Add tests for formatMoney

--- src/test/models/NumberFormatter/formatMoney.test.ts
+++ src/test/models/NumberFormatter/formatMoney.test.ts
@@ -1,6 +1,10 @@
 import { NumberFormatter } from "$models/NumberFormatter";
 
 describe("formatMoney", () => {
-  it("Executes formatNumber and adds a $ before", () =>
+  it("executes formatNumber and adds a $ before", () =>
     expect(NumberFormatter.formatMoney(12345.67888)).toEqual("$12.345,679"));
+
+  it("formats 0", () => expect(NumberFormatter.formatMoney(0)).toEqual("$0"));
+
+  it("formats a negative number", () => expect(NumberFormatter.formatMoney(-12)).toEqual("$-12"));
 });
