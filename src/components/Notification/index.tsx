cd1d3552be6a88ab06bdc6c60d69e37dba7edab7
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-08 23:38:36 -0300

    Add typenames constants

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -1,9 +1,10 @@
 import React, { Fragment, FunctionComponent } from "react";
 import { TNotification } from "$hooks";
+import { JOB_APPLICATION_NOTIFICATION } from "$typenames";
 import { JobApplicationNotification } from "./JobApplicationNotification";
 
 export const Notification: FunctionComponent<IContainerProps> = ({ className, notification }) => {
-  if (notification.__typename === "JobApplicationNotification") {
+  if (notification.__typename === JOB_APPLICATION_NOTIFICATION) {
     return <JobApplicationNotification className={className} notification={notification} />;
   }
   return <Fragment />;
--- src/models/hooks/queries/useNotifications.ts
+++ src/models/hooks/queries/useNotifications.ts
@@ -1,6 +1,7 @@
 import { GET_NOTIFICATIONS } from "$queries";
 import { usePaginatedQuery } from "$hooks";
 import { IJobApplication } from "$interfaces/JobApplication";
+import { JOB_APPLICATION_NOTIFICATION_TYPE } from "$typenames";
 
 export const useNotifications = () =>
   usePaginatedQuery<{}, TNotification>({
@@ -13,7 +14,7 @@ export const useNotifications = () =>
 export type TNotification = IJobApplicationNotification;
 
 export interface IJobApplicationNotification {
-  __typename: "JobApplicationNotification";
+  __typename: JOB_APPLICATION_NOTIFICATION_TYPE;
   uuid: string;
   message: string;
   createdAt: string;
