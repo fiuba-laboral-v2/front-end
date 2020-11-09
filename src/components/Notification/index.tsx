b66bc72c46d8dc9e04be433f0aba4ad0735cc9be
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-09 01:33:41 -0300

    Rename interface

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -3,14 +3,14 @@ import { TNotification } from "$interfaces/Notification";
 import { JOB_APPLICATION_NOTIFICATION } from "$typenames";
 import { JobApplicationNotification } from "./JobApplicationNotification";
 
-export const Notification: FunctionComponent<IContainerProps> = ({ className, notification }) => {
+export const Notification: FunctionComponent<IComponentProps> = ({ className, notification }) => {
   if (notification.__typename === JOB_APPLICATION_NOTIFICATION) {
     return <JobApplicationNotification className={className} notification={notification} />;
   }
   return <Fragment />;
 };
 
-interface IContainerProps {
+interface IComponentProps {
   className?: string;
   notification: TNotification;
 }
