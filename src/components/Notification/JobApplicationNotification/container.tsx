995e2fa0bb07cc97bd0e7941c72839e33b482125
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-09 01:38:04 -0300

    Avoid rendering spinner

--- src/components/Notification/JobApplicationNotification/container.tsx
+++ src/components/Notification/JobApplicationNotification/container.tsx
@@ -4,15 +4,12 @@ import { RoutesBuilder } from "$models/RoutesBuilder";
 
 import { ApplicantJobApplicationNotification } from "./ApplicantJobApplicationNotification";
 import { CompanyJobApplicationNotification } from "./CompanyJobApplicationNotification";
-import { LoadingSpinner } from "$components/LoadingSpinner";
 import { Redirect } from "$components/Redirect";
 
 import { IContainerProps } from "./interfaces";
 
 export const JobApplicationNotificationContainer: FunctionComponent<IContainerProps> = props => {
   const response = useCurrentUser();
-
-  if (response.loading) return <LoadingSpinner />;
   if (response.error) return <Redirect to={RoutesBuilder.public.internalServerError()} />;
 
   const currentUser = response.data.getCurrentUser;
