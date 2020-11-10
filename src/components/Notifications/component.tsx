93dc253036429f4f2114a150a6b110490af17db6
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-10 01:31:45 -0300

    Use alias import

--- src/components/Notifications/component.tsx
+++ src/components/Notifications/component.tsx
@@ -2,9 +2,9 @@ import React, { FunctionComponent } from "react";
 import { List } from "$components/List";
 import { Window } from "$components/Window";
 import { Notification } from "$components/Notification";
+import { Title } from "$components/Title";
 import { IComponentProps } from "./interfaces";
 import styles from "./styles.module.scss";
-import { Title } from "../Title";
 
 export const Notifications: FunctionComponent<IComponentProps> = ({
   translations,
