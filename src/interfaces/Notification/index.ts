d148260257c77dce89d6f8474ee7d8237905533c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-11 01:19:34 -0300

    Add isNew property to getNotifications query an TNotification type

--- src/interfaces/Notification/index.ts
+++ src/interfaces/Notification/index.ts
@@ -7,6 +7,7 @@ export interface IJobApplicationNotification {
   __typename: JOB_APPLICATION_NOTIFICATION_TYPE;
   uuid: string;
   message: string;
+  isNew: boolean;
   createdAt: string;
   adminEmail: string;
   jobApplication: IJobApplication;
