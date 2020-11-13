0bf927be5085a68a6f1f04512d452c11fe04066f
Dylan Alvarez <dylanalvarez1995@gmail.com>
2020-11-13 01:52:36 -0300

    Fix loading admin task list width

--- src/pages/Admin/Home/Dashboard/TaskList/component.tsx
+++ src/pages/Admin/Home/Dashboard/TaskList/component.tsx
@@ -10,17 +10,19 @@ export const TaskList: FunctionComponent<ITaskListProps> = ({
   statuses,
   translations,
   ...props
-}) => {
-  if (!adminTasks) return <LoadingSpinner />;
-
-  return (
-    <div className={styles.taskList}>
-      <div className={styles.info}>
-        <ListInfo statuses={statuses} translations={translations} {...props} />
-      </div>
-      <div className={styles.content}>
-        <ListBody adminTasks={adminTasks} {...props} />
-      </div>
-    </div>
-  );
-};
+}) => (
+  <div className={styles.taskList}>
+    {adminTasks ? (
+      <>
+        <div className={styles.info}>
+          <ListInfo statuses={statuses} translations={translations} {...props} />
+        </div>
+        <div className={styles.content}>
+          <ListBody adminTasks={adminTasks} {...props} />
+        </div>
+      </>
+    ) : (
+      <LoadingSpinner className={styles.spinner} />
+    )}
+  </div>
+);
