e0f453f1740d7bfb28051fb5b4e6d7ae4b90ae98
Dylan Alvarez <dylanalvarez1995@gmail.com>
2020-11-13 01:04:28 -0300

    Fix application layout in admin dashboard

--- src/components/CareersSection/component.tsx
+++ src/components/CareersSection/component.tsx
@@ -3,14 +3,16 @@ import { ICareersProps } from "./interfaces";
 import styles from "./styles.module.scss";
 import { Subtitle } from "$components/Subtitle";
 import { CareersDetail } from "./CareersDetail";
+import classNames from "classnames";
 
 export const CareersSection: FunctionComponent<ICareersProps> = ({
   className,
   careers,
+  mobileLayout,
   translations
 }) => (
   <div className={className}>
-    <section className={styles.items}>
+    <section className={classNames(styles.items, { [styles.mobile]: mobileLayout })}>
       <Subtitle>{translations.careersTitle}</Subtitle>
       <CareersDetail
         careers={careers}
