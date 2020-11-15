117142003ac55fd47e393a341760edd613c400c5
Dylan Alvarez <dylanalvarez1995@gmail.com>
2020-11-15 19:40:02 -0300

    Move function inside of component

--- src/components/Fields/SelectField/index.tsx
+++ src/components/Fields/SelectField/index.tsx
@@ -7,8 +7,6 @@ import { isNaN } from "lodash";
 import classNames from "classnames";
 import styles from "./styles.module.scss";
 
-const valueToString = (value: IBaseValue) => (isNaN(value) ? "" : value.toString());
-
 export const SelectField = <Value extends IBaseValue>({
   mandatory,
   fieldName,
@@ -16,45 +14,49 @@ export const SelectField = <Value extends IBaseValue>({
   title,
   className,
   helperText
-}: ISelectFieldProps<Value>) => (
-  <FastField
-    name={fieldName}
-    validate={(value: number) => FormikValidator({ mandatory })(valueToString(value))}
-  >
-    {({ meta, form }: FastFieldProps<Value>) => {
-      const error = meta.touched && meta.error;
-      const stringValue = valueToString(meta.value);
-      return (
-        <>
-          <FormControl className={classNames(styles.container, className)} error={!!error}>
-            <InputLabel required={mandatory} htmlFor={fieldName}>
-              {title}
-            </InputLabel>
-            <Select
-              className={styles.select}
-              id={fieldName}
-              onBlur={() => form.setFieldTouched(fieldName, true)}
-              onChange={event => form.setFieldValue(fieldName, event.target.value)}
-              value={stringValue}
-              native
-            >
-              {isEmpty(stringValue) && <option value="" />}
-              {options.map(option => {
-                const stringOptionValue = option.value.toString();
-                return (
-                  <option key={stringOptionValue} value={stringOptionValue}>
-                    {option.label}
-                  </option>
-                );
-              })}
-            </Select>
-            <FormHelperText>{error || helperText}</FormHelperText>
-          </FormControl>
-        </>
-      );
-    }}
-  </FastField>
-);
+}: ISelectFieldProps<Value>) => {
+  const valueToString = (value: IBaseValue) => (isNaN(value) ? "" : value.toString());
+
+  return (
+    <FastField
+      name={fieldName}
+      validate={(value: number) => FormikValidator({ mandatory })(valueToString(value))}
+    >
+      {({ meta, form }: FastFieldProps<Value>) => {
+        const error = meta.touched && meta.error;
+        const stringValue = valueToString(meta.value);
+        return (
+          <>
+            <FormControl className={classNames(styles.container, className)} error={!!error}>
+              <InputLabel required={mandatory} htmlFor={fieldName}>
+                {title}
+              </InputLabel>
+              <Select
+                className={styles.select}
+                id={fieldName}
+                onBlur={() => form.setFieldTouched(fieldName, true)}
+                onChange={event => form.setFieldValue(fieldName, event.target.value)}
+                value={stringValue}
+                native
+              >
+                {isEmpty(stringValue) && <option value="" />}
+                {options.map(option => {
+                  const stringOptionValue = option.value.toString();
+                  return (
+                    <option key={stringOptionValue} value={stringOptionValue}>
+                      {option.label}
+                    </option>
+                  );
+                })}
+              </Select>
+              <FormHelperText>{error || helperText}</FormHelperText>
+            </FormControl>
+          </>
+        );
+      }}
+    </FastField>
+  );
+};
 
 interface ISelectFieldProps<Value> {
   className?: string;
