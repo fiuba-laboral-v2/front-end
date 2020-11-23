a54e4a837e7d5a7cfdaa97475fdc12327b226b83
Dylan Alvarez <dylanalvarez1995@gmail.com>
2020-11-23 03:06:47 -0300

    Optimize company edit page

--- src/pages/Company/EditableProfile/component.tsx
+++ src/pages/Company/EditableProfile/component.tsx
@@ -1,50 +1,34 @@
 import React, { FunctionComponent } from "react";
-
-import { Form as FormikForm } from "formik";
-import { FormikHelpers } from "formik/dist/types";
-
 import { FormFooter } from "$components/FormFooter";
 import { CompanyDataFormSection } from "./CompanyDataFormSection";
 import { ContactInformationFormSection } from "$components/ContactInformationFormSection";
 import { Form } from "$components/Form";
-import { Formik } from "$components/Formik";
-
 import { IEditableProfileFormValues, IEditableProfileTranslations } from "./interfaces";
+import { FormikProps } from "formik";
 import styles from "./styles.module.scss";
 
 export const EditableProfile: FunctionComponent<IEditableProfileProps> = ({
-  initialValues,
-  onUpdate,
   translations,
-  acceptanceCriteria
+  acceptanceCriteria,
+  formikProps: { setFieldValue, values, isSubmitting, errors }
 }) => (
-  <Form title={translations.title} acceptanceCriteria={acceptanceCriteria}>
-    <Formik initialValues={initialValues} onSubmit={onUpdate}>
-      {({ values, setFieldValue, isSubmitting, errors }) => (
-        <FormikForm>
-          <CompanyDataFormSection
-            className={styles.formSection}
-            setFieldValue={setFieldValue}
-            initialLogo={values.logo}
-          />
-          <ContactInformationFormSection className={styles.formSection} />
-          <FormFooter
-            isSubmitting={isSubmitting}
-            submitButtonText={translations.submit}
-            errors={errors}
-          />
-        </FormikForm>
-      )}
-    </Formik>
+  <Form title={translations?.title} acceptanceCriteria={acceptanceCriteria}>
+    <CompanyDataFormSection
+      className={styles.formSection}
+      setFieldValue={setFieldValue}
+      initialLogo={values.logo}
+    />
+    <ContactInformationFormSection className={styles.formSection} />
+    <FormFooter
+      isSubmitting={isSubmitting}
+      submitButtonText={translations?.submit}
+      errors={errors}
+    />
   </Form>
 );
 
 interface IEditableProfileProps {
-  acceptanceCriteria: string;
-  initialValues: IEditableProfileFormValues;
-  translations: IEditableProfileTranslations;
-  onUpdate: (
-    values: IEditableProfileFormValues,
-    formikHelpers: FormikHelpers<IEditableProfileFormValues>
-  ) => void | Promise<any>;
+  acceptanceCriteria?: string;
+  translations?: IEditableProfileTranslations;
+  formikProps: FormikProps<IEditableProfileFormValues>;
 }
--- src/pages/Company/EditableProfile/container.tsx
+++ src/pages/Company/EditableProfile/container.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useCallback } from "react";
 import { useHistory } from "react-router-dom";
 import { FormikHelpers } from "formik";
 import { useMyCompanyProfile, useTranslations, useUpdateCurrentCompany } from "$hooks";
@@ -9,56 +9,67 @@ import { RoutesBuilder } from "$models/RoutesBuilder";
 import { saveCompanyErrorHandlers } from "$errorHandlers/saveCompanyErrorHandlers";
 import { useSnackbar } from "$hooks/snackbar/useSnackbar";
 import { Window } from "$components/Window";
-import { LoadingWindow } from "$components/LoadingWindow";
+import { Formik } from "$components/Formik";
+import { FormikForm } from "$components/FormikForm";
+import { ICompany } from "$interfaces/Company";
 
 export const EditableProfileContainer: FunctionComponent = () => {
   const history = useHistory();
   const { enqueueSnackbar } = useSnackbar();
   const { updateCurrentCompany } = useUpdateCurrentCompany();
   const companyProfile = useMyCompanyProfile();
-  const company = companyProfile.data?.getCurrentUser.company;
-
   const translations = useTranslations<IEditableProfileTranslations>("editMyCompanyProfile");
   const acceptanceCriteria = useTranslations<{ text: string }>("companyEditableAcceptanceCriteria");
 
+  const company = companyProfile.data?.getCurrentUser.company;
+
+  const modelToValues = useCallback((model?: ICompany) => {
+    const values: IEditableProfileFormValues = {
+      uuid: model?.uuid || "",
+      companyName: model?.companyName,
+      slogan: model?.slogan,
+      description: model?.description,
+      logo: model?.logo,
+      website: model?.website,
+      email: model?.email,
+      phoneNumbers: model?.phoneNumbers,
+      photos: model?.photos,
+      _form: ""
+    };
+    return values;
+  }, []);
+
+  const onSubmit = useCallback(
+    async (
+      { _form, ...companyValues }: IEditableProfileFormValues,
+      { setErrors, setSubmitting }: FormikHelpers<IEditableProfileFormValues>
+    ) => {
+      const updateCompanyResult = await updateCurrentCompany({
+        variables: companyValues,
+        errorHandlers: saveCompanyErrorHandlers({ setErrors, enqueueSnackbar })
+      });
+      if (updateCompanyResult.error) return;
+
+      setSubmitting(false);
+      history.push(RoutesBuilder.company.myProfile());
+    },
+    [enqueueSnackbar, history, updateCurrentCompany]
+  );
+
   if (companyProfile.error) return <Redirect to={RoutesBuilder.public.internalServerError()} />;
-  if (!translations || !companyProfile || !acceptanceCriteria || !company) {
-    return <LoadingWindow />;
-  }
-
-  const onUpdate = async (
-    { _form, ...companyValues }: IEditableProfileFormValues,
-    { setErrors, setSubmitting }: FormikHelpers<IEditableProfileFormValues>
-  ) => {
-    const updateCompanyResult = await updateCurrentCompany({
-      variables: companyValues,
-      errorHandlers: saveCompanyErrorHandlers({ setErrors, enqueueSnackbar })
-    });
-    if (updateCompanyResult.error) return;
-
-    setSubmitting(false);
-    history.push(RoutesBuilder.company.myProfile());
-  };
 
   return (
-    <Window>
-      <EditableProfile
-        acceptanceCriteria={acceptanceCriteria.text}
-        initialValues={{
-          uuid: company.uuid,
-          companyName: company.companyName,
-          slogan: company.slogan,
-          description: company.description,
-          logo: company.logo,
-          website: company.website,
-          email: company.email,
-          phoneNumbers: company.phoneNumbers,
-          photos: company.photos,
-          _form: ""
-        }}
-        translations={translations}
-        onUpdate={onUpdate}
-      />
+    <Window loading={!translations || !companyProfile || !acceptanceCriteria || !company}>
+      <Formik initialValues={modelToValues()} {...{ onSubmit }}>
+        {formikProps => (
+          <FormikForm initialValuesModel={company} {...{ modelToValues, formikProps }}>
+            <EditableProfile
+              acceptanceCriteria={acceptanceCriteria?.text}
+              {...{ translations, formikProps }}
+            />
+          </FormikForm>
+        )}
+      </Formik>
     </Window>
   );
 };
