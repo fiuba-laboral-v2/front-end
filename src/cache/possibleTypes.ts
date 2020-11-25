addcd361bddaef04532a9d19224a140ae929699d
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-25 08:38:45 -0300

    Add CompanyNotification's posible types

--- src/cache/possibleTypes.ts
+++ src/cache/possibleTypes.ts
@@ -1,5 +1,6 @@
 import { PossibleTypesMap } from "@apollo/client";
 
 export const possibleTypes: PossibleTypesMap = {
-  AdminTask: ["Company", "Applicant", "Offer", "JobApplication"]
+  AdminTask: ["Company", "Applicant", "Offer", "JobApplication"],
+  CompanyNotification: ["CompanyNewJobApplicationNotification"]
 };
