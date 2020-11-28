d259bb5923259367f0d533fec635d0953c2362c2
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-28 20:06:18 -0300

    Add translations for snackbar

--- src/components/CompanyNotificationsIcon/container.tsx
+++ src/components/CompanyNotificationsIcon/container.tsx
@@ -1,20 +1,28 @@
-import React, { FunctionComponent, useEffect } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { useUnreadCompanyNotifications, useShowInfo } from "$hooks";
+import { useUnreadCompanyNotifications, useShowInfo, useTranslations } from "$hooks";
 import { NotificationsIcon } from "$components/NotificationsIcon";
 import { RoutesBuilder } from "$models/RoutesBuilder";
 
 export const CompanyNotificationsIconContainer: FunctionComponent = () => {
+  const [lastUnreadValue, setLastUnreadValue] = useState<boolean | undefined>(undefined);
   const history = useHistory();
+  const translations = useTranslations<ITranslations>("notificationsNotice");
   const unread = useUnreadCompanyNotifications();
   const showInfo = useShowInfo();
   useEffect(() => {
     showInfo({
       action: () => history.push(RoutesBuilder.company.notifications()),
-      message: "Tenés notificaciones",
-      actionMessage: "Ver",
-      skip: !unread
+      message: translations?.message || "",
+      actionMessage: translations?.actionMessage || "",
+      skip: !unread || !!lastUnreadValue
     });
-  }, [unread]);
+    setLastUnreadValue(unread);
+  }, [lastUnreadValue, unread, showInfo, history, translations]);
   return <NotificationsIcon unread={unread} />;
 };
+
+interface ITranslations {
+  message: string;
+  actionMessage: string;
+}
