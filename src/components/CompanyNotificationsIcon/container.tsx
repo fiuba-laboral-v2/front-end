4cc2c3a17813cc0ab04af06cd87d8002e0ceadab
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-28 19:21:28 -0300

    Show snackbar when there are unread notifications

--- src/components/CompanyNotificationsIcon/container.tsx
+++ src/components/CompanyNotificationsIcon/container.tsx
@@ -1,8 +1,20 @@
-import React, { FunctionComponent } from "react";
-import { useUnreadCompanyNotifications } from "$hooks";
+import React, { FunctionComponent, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { useUnreadCompanyNotifications, useShowInfo } from "$hooks";
 import { NotificationsIcon } from "$components/NotificationsIcon";
+import { RoutesBuilder } from "$models/RoutesBuilder";
 
 export const CompanyNotificationsIconContainer: FunctionComponent = () => {
+  const history = useHistory();
   const unread = useUnreadCompanyNotifications();
+  const showInfo = useShowInfo();
+  useEffect(() => {
+    showInfo({
+      action: () => history.push(RoutesBuilder.company.notifications()),
+      message: "Tenés notificaciones",
+      actionMessage: "Ver",
+      skip: !unread
+    });
+  }, [unread]);
   return <NotificationsIcon unread={unread} />;
 };
