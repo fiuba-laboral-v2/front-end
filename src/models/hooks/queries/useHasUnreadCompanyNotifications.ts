4e970f9d1ebb6b517be4a0dda83caf81b968821d
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-11-29 16:35:52 -0300

    USe no-cache in the useHasUnreadCompanyNotifications hook

--- src/models/hooks/queries/useHasUnreadCompanyNotifications.ts
+++ src/models/hooks/queries/useHasUnreadCompanyNotifications.ts
@@ -3,6 +3,7 @@ import { HAS_UNREAD_COMPANY_NOTIFICATIONS } from "$queries";
 
 export const useHasUnreadCompanyNotifications = () => {
   const response = useQuery<{}, IResponse>(HAS_UNREAD_COMPANY_NOTIFICATIONS, {
+    fetchPolicy: "no-cache",
     pollInterval: 30000
   });
   return response.data && response.data.hasUnreadCompanyNotifications;
