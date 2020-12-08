d16aab20dda2b4f1efb1d32641f11c5bfe41d84f
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-08 19:06:41 -0300

    Add Applicant notification link in the navbar

--- src/models/NavBarLinks/ApplicantNavBarLinks.ts
+++ src/models/NavBarLinks/ApplicantNavBarLinks.ts
@@ -6,8 +6,9 @@ import { getTooltipMessage } from "./getTooltipMessage";
 import LibraryBooksIcon from "@material-ui/icons/LibraryBooks";
 import SchoolIcon from "@material-ui/icons/School";
 import BusinessIcon from "@material-ui/icons/Business";
+import { NotificationsIcon } from "$components/NotificationsIcon";
 
-const { offerList, myProfile, companies } = RoutesBuilder.applicant;
+const { offerList, myProfile, companies, notifications } = RoutesBuilder.applicant;
 
 export const ApplicantNavBarLinks = {
   create: (currentUser: TCurrentUser, translations: INavBarTranslations): INavBarLink[] => [
@@ -28,6 +29,12 @@ export const ApplicantNavBarLinks = {
       title: translations.companies,
       tooltipMessage: getTooltipMessage(currentUser, translations, companies()),
       icon: BusinessIcon
+    },
+    {
+      path: notifications(),
+      title: translations.notifications,
+      tooltipMessage: getTooltipMessage(currentUser, translations, companies()),
+      icon: NotificationsIcon
     }
   ]
 };
