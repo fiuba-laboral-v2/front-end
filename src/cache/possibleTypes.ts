dfe091b42a0c4877ce32d3235d44dafb0ba56011
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-10 02:03:51 -0300

    Add ApplicantNotification possibleTypes

--- src/cache/possibleTypes.ts
+++ src/cache/possibleTypes.ts
@@ -2,5 +2,6 @@ import { PossibleTypesMap } from "@apollo/client";
 
 export const possibleTypes: PossibleTypesMap = {
   AdminTask: ["Company", "Applicant", "Offer", "JobApplication"],
-  CompanyNotification: ["NewJobApplicationCompanyNotification", "ApprovedOfferCompanyNotification"]
+  CompanyNotification: ["NewJobApplicationCompanyNotification", "ApprovedOfferCompanyNotification"],
+  ApplicantNotification: ["ApprovedJobApplicationApplicantNotification"]
 };
