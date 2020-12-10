5b94b17cf209d4c10f6ea745d04b1163ab122ea5
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-09 23:28:51 -0300

    Remove translations from NotificationIcon

--- src/components/NotificationsIcon/container.tsx
+++ src/components/NotificationsIcon/container.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent, useEffect, useState } from "react";
 import { useHistory } from "react-router-dom";
-import { useInfoSnackbar, useTranslations } from "$hooks";
+import { useInfoSnackbar } from "$hooks";
 import { NotificationsIcon } from "./component";
 
 export const NotificationIconContainer: FunctionComponent<IContainerProps> = ({
@@ -9,27 +9,21 @@ export const NotificationIconContainer: FunctionComponent<IContainerProps> = ({
 }) => {
   const [lastUnreadValue, setLastUnreadValue] = useState(false);
   const history = useHistory();
-  const translations = useTranslations<ITranslations>("notificationsNotice");
   const hasUnreadNotifications = useHasUnreadNotifications();
   const infoSnackbar = useInfoSnackbar();
   useEffect(() => {
     infoSnackbar({
       action: () => history.push(actionRoute),
-      message: translations?.message || "Tenés notificaciones",
-      actionMessage: translations?.actionMessage || "Ver",
+      message: "Tenés notificaciones",
+      actionMessage: "Ver",
       skip: !hasUnreadNotifications || lastUnreadValue
     });
     setLastUnreadValue(!!hasUnreadNotifications);
-  }, [lastUnreadValue, hasUnreadNotifications, infoSnackbar, history, translations, actionRoute]);
+  }, [lastUnreadValue, hasUnreadNotifications, infoSnackbar, history, actionRoute]);
   return <NotificationsIcon unread={hasUnreadNotifications} />;
 };
 
 export interface IContainerProps {
   useHasUnreadNotifications: () => boolean | undefined;
   actionRoute: string;
-}
-
-interface ITranslations {
-  message: string;
-  actionMessage: string;
 }
