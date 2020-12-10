6c6b247ce261818a6ba186c0602df0fce0187fc6
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-09 21:42:59 -0300

    Use NODE_ENV method from Environment in the env

--- src/config/Environment.ts
+++ src/config/Environment.ts
@@ -2,6 +2,6 @@ type Env = "production" | "staging" | "development" | "test";
 
 export const Environment = {
   NODE_ENV: () => process.env.NODE_ENV,
-  env: () => (process.env.REACT_APP_STAGE || process.env.NODE_ENV) as Env,
+  env: () => (process.env.REACT_APP_STAGE || Environment.NODE_ENV()) as Env,
   publicUrl: () => process.env.PUBLIC_URL
 };
