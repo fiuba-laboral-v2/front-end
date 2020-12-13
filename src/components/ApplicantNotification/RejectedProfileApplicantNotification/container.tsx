bcb225083b00de9e93db12a66fc2201f553178db
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-13 19:59:12 -0300

    Add RejectionReason in the RejectedProfileApplicantNotification component

--- src/components/ApplicantNotification/RejectedProfileApplicantNotification/container.tsx
+++ src/components/ApplicantNotification/RejectedProfileApplicantNotification/container.tsx
@@ -2,6 +2,8 @@ import React, { FunctionComponent } from "react";
 import { useTranslations } from "$hooks";
 
 import { Notification } from "$components/Notification";
+import { RejectionReason } from "$components/Notification/RejectionReason";
+import { NotificationBody } from "$components/Notification/NotificationBody";
 import { NotificationTitle } from "$components/Notification/NotificationTitle";
 import { JobApplicationIcon } from "$components/Icons/JobApplicationIcon";
 import { StatusIcon } from "$components/StatusIcon";
@@ -15,6 +17,8 @@ export const RejectedProfileApplicantNotificationContainer: FunctionComponent<IC
 }) => {
   const translations = useTranslations<{ title: string }>("rejectedProfileApplicantNotification");
 
+  const { adminEmail, moderatorMessage } = notification;
+
   return (
     <Notification
       className={className}
@@ -22,6 +26,9 @@ export const RejectedProfileApplicantNotificationContainer: FunctionComponent<IC
       icon={<StatusIcon Icon={JobApplicationIcon} approvalStatus={ApprovalStatus.rejected} />}
     >
       <NotificationTitle>{translations?.title || ""}</NotificationTitle>
+      <NotificationBody>
+        <RejectionReason moderatorEmail={adminEmail} message={moderatorMessage} />
+      </NotificationBody>
     </Notification>
   );
 };
