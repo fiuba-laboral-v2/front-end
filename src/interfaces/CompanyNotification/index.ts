0584c25254b98b2087b4908152512a2890f8f005
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-14 01:39:31 -0300

    Add IApprovedProfileCompanyNotification

--- src/interfaces/CompanyNotification/index.ts
+++ src/interfaces/CompanyNotification/index.ts
@@ -1,7 +1,8 @@
 import {
   NEW_JOB_APPLICATION_COMPANY_NOTIFICATION_TYPE,
   APPROVED_OFFER_COMPANY_NOTIFICATION_TYPE,
-  REJECTED_OFFER_COMPANY_NOTIFICATION_TYPE
+  REJECTED_OFFER_COMPANY_NOTIFICATION_TYPE,
+  APPROVED_PROFILE_COMPANY_NOTIFICATION_TYPE
 } from "$typenames";
 import { IJobApplication } from "$interfaces/JobApplication";
 import { IOffer } from "$interfaces/Offer";
@@ -33,4 +34,8 @@ export interface IRejectedOfferCompanyNotification extends ICommonAttributes {
   __typename: REJECTED_OFFER_COMPANY_NOTIFICATION_TYPE;
   moderatorMessage: string;
   offer: IOffer;
+}
+
+export interface IApprovedProfileCompanyNotification extends ICommonAttributes {
+  __typename: APPROVED_PROFILE_COMPANY_NOTIFICATION_TYPE;
 }
