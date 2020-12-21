f240989a45dbea2976e696a2bf571b7cf3d3364f
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-21 05:03:48 -0300

    Redirect to company user form when clicking button

--- src/pages/Company/CompanyUsers/container.tsx
+++ src/pages/Company/CompanyUsers/container.tsx
@@ -1,15 +1,20 @@
 import React, { FunctionComponent } from "react";
+import { useHistory } from "react-router-dom";
 import { ICompanyUser } from "$interfaces/CompanyUser";
-import { useCompanyUsers } from "$hooks";
+import { RoutesBuilder } from "$models/RoutesBuilder";
+import { useCompanyUsers, useTranslations } from "$hooks";
 
 import { ListPageContainer } from "$components/ListPageContainer";
 import { ListHeader } from "./ListHeader";
+import { Button } from "$components/Button";
 import { ListContentItem } from "./ListContentItem";
 
 import styles from "./styles.module.scss";
 
 export const CompanyUsers: FunctionComponent = () => {
+  const history = useHistory();
   const response = useCompanyUsers();
+  const translations = useTranslations<ITranslation>("companyUsers");
   const companyUsers = response?.data?.getCompanyUsers.results;
 
   return (
@@ -23,6 +28,14 @@ export const CompanyUsers: FunctionComponent = () => {
       fetchMore={response.fetchMore}
       shouldFetchMore={response.data?.getCompanyUsers.shouldFetchMore}
       loading={response.loading}
-    />
+    >
+      <Button onClick={() => history.push(RoutesBuilder.company.createUser())} kind="primary">
+        {translations?.addAdminButtonLabel || ""}
+      </Button>
+    </ListPageContainer>
   );
 };
+
+interface ITranslation {
+  addAdminButtonLabel: string;
+}
