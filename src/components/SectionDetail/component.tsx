2406ad4187867e2e706ac41d07e3cbdee454b432
Dylan Alvarez <dylanalvarez1995@gmail.com>
2020-12-21 04:32:41 -0300

    Add missing titles to applicant detail

--- src/components/SectionDetail/component.tsx
+++ src/components/SectionDetail/component.tsx
@@ -1,9 +1,14 @@
 import React, { FunctionComponent } from "react";
 import { Subtitle } from "$components/Subtitle";
 import styles from "./styles.module.scss";
+import classNames from "classnames";
 
-export const SectionDetail: FunctionComponent<ISectionDetailProps> = ({ title, text }) => (
-  <div className={styles.section}>
+export const SectionDetail: FunctionComponent<ISectionDetailProps> = ({
+  title,
+  text,
+  className
+}) => (
+  <div className={classNames(styles.section, className)}>
     <Subtitle>{title}</Subtitle>
     <p className={styles.text}>{text}</p>
   </div>
@@ -12,4 +17,5 @@ export const SectionDetail: FunctionComponent<ISectionDetailProps> = ({ title, t
 interface ISectionDetailProps {
   title: string;
   text: string;
+  className?: string;
 }
