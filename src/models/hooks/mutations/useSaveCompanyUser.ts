c6cd5ab6ef713a02fbd3816dd4f86193d6dde06f
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-22 22:50:15 -0300

    Update cache when adding a new companyUser

--- src/models/hooks/mutations/useSaveCompanyUser.ts
+++ src/models/hooks/mutations/useSaveCompanyUser.ts
@@ -1,5 +1,7 @@
 import { SAVE_COMPANY_USER } from "$mutations";
-import { useMutation } from "$hooks";
+import { GET_COMPANY_USERS } from "$queries";
+import { IMutationOptions, useMutation } from "$hooks";
+import { IPaginatedResult } from "$hooks/queries/interfaces";
 import { ICompanyUser } from "$interfaces/CompanyUser";
 import { ICompanyUserInput } from "$interfaces/User";
 
@@ -7,7 +9,41 @@ export const useSaveCompanyUser = () => {
   const { mutation, ...result } = useMutation<ISaveCompanyUserInput, IResponseProps>(
     SAVE_COMPANY_USER
   );
-  return { saveCompanyUser: mutation, ...result };
+
+  const saveCompanyUser = ({
+    variables,
+    ...options
+  }: IMutationOptions<IResponseProps, ISaveCompanyUserInput>) =>
+    mutation({
+      variables,
+      ...options,
+      update: (cache, { data }) => {
+        cache.modify({
+          fields: {
+            getCompanyUsers: (existingCompanyUsers: IPaginatedResult<ICompanyUser>) => {
+              const newTodoRef = cache.writeQuery({
+                data: {
+                  getCompanyUsers: {
+                    results: data?.saveCompanyUser,
+                    shouldFetchMore: true
+                  }
+                },
+                query: GET_COMPANY_USERS
+              });
+              return {
+                ...existingCompanyUsers,
+                results: {
+                  newTodoRef,
+                  ...existingCompanyUsers.results
+                }
+              };
+            }
+          }
+        });
+      }
+    });
+
+  return { saveCompanyUser, ...result };
 };
 
 interface IResponseProps {
