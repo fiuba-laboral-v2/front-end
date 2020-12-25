7c98ea1a997fc2efe5e6557a720e6e4004543f12
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-25 00:06:56 -0300

    Integrate passwordRecovery in the company login

--- src/pages/Company/routes.tsx
+++ src/pages/Company/routes.tsx
@@ -51,7 +51,7 @@ export const CompanyRoutes = [
   { path: users(), component: CompanyUsers },
   { path: createUser(), component: AddCompanyUser },
   { path: editPassword(), component: EditPassword },
-  { path: passwordRecovery(), component: PasswordRecovery },
+  { path: passwordRecovery(), component: PasswordRecovery, public: true },
   {
     path: editMyForgottenPassword(),
     component: (props: { location: Location }) => (
