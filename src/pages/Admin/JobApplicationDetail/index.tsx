f5f259ccc030191bb0ce9f16bd9083d647d1117c
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-26 16:14:25 -0300

    Fix jobApplicationDetail style

--- src/pages/Admin/JobApplicationDetail/index.tsx
+++ src/pages/Admin/JobApplicationDetail/index.tsx
@@ -12,7 +12,7 @@ import styles from "./styles.module.scss";
 export const JobApplicationDetail: FunctionComponent = () => {
   const { uuid } = useParams<{ uuid: string }>();
   return (
-    <Window>
+    <Window className={styles.taskDetail}>
       <JobApplicationDetailInfo selectedTaskUuid={uuid} />
       <JobApplicationDetailContent className={styles.content} jobApplicationUuid={uuid} />
     </Window>
