9f9e308eca566c2506bdea77c0d34c653367b9e3
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-25 21:14:27 -0300

    Use IconButton in the OpenDetailIcon

--- src/components/OpenDetailIcon/index.tsx
+++ src/components/OpenDetailIcon/index.tsx
@@ -1,19 +1,26 @@
 import React, { FunctionComponent } from "react";
+import { useTranslations } from "$hooks";
 import { useHistory } from "react-router-dom";
+import { IconButton } from "$components/IconButton";
+import Tooltip from "@material-ui/core/Tooltip";
 import OpenInNewIcon from "@material-ui/icons/OpenInNew";
-import styles from "./styles.module.scss";
 
 export const OpenDetailIcon: FunctionComponent<IListContentItemProps> = ({ detailRoute }) => {
+  const translations = useTranslations<ITranslations>("openDetailIcon");
   const history = useHistory();
   return (
-    <OpenInNewIcon
-      className={styles.icon}
-      fontSize="small"
-      onClick={() => history.push(detailRoute)}
-    />
+    <Tooltip title={translations?.seeDetailTooltipMessage || ""}>
+      <div>
+        <IconButton Icon={OpenInNewIcon} onClick={() => history.push(detailRoute)} />
+      </div>
+    </Tooltip>
   );
 };
 
 interface IListContentItemProps {
   detailRoute: string;
+}
+
+interface ITranslations {
+  seeDetailTooltipMessage: string;
 }
