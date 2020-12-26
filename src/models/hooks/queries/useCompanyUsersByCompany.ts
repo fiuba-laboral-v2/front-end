e546bc25d2b3a726bc038070bb37e0d6751d9e55
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2020-12-26 02:59:23 -0300

    Add companyUuid variable to the useCompanyUsersByCompany hook

--- src/models/hooks/queries/useCompanyUsersByCompany.ts
+++ src/models/hooks/queries/useCompanyUsersByCompany.ts
@@ -2,10 +2,10 @@ import { usePaginatedQuery } from "$hooks";
 import { GET_COMPANY_USERS_BY_COMPANY } from "$queries";
 import { ICompanyUser } from "$interfaces/CompanyUser";
 
-export const useCompanyUsersByCompany = () =>
+export const useCompanyUsersByCompany = (companyUuid: string) =>
   usePaginatedQuery<{}, ICompanyUser>({
     documentNode: GET_COMPANY_USERS_BY_COMPANY,
     queryName: "getCompanyUsersByCompany",
-    variables: {},
+    variables: { companyUuid },
     timestampKey: "updatedAt"
   });
--- src/pages/Admin/CompanyUsers/container.tsx
+++ src/pages/Admin/CompanyUsers/container.tsx
@@ -1,10 +1,12 @@
 import React, { FunctionComponent } from "react";
+import { useParams } from "react-router-dom";
 import { useCompanyUsersByCompany } from "$hooks";
 
 import { CompanyUsersTable } from "$components/CompanyUsersTable";
 
 export const CompanyUsersContainer: FunctionComponent = () => {
-  const response = useCompanyUsersByCompany();
+  const { companyUuid } = useParams<{ companyUuid: string }>();
+  const response = useCompanyUsersByCompany(companyUuid);
 
   return (
     <CompanyUsersTable
