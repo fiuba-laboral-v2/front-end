95b86e6c7e94b94e14033f1c0eb34c02bea575cd
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-03 16:38:39 -0300

    Add getCurrentRole method to the CurrentUser

--- src/models/CurrentUser.ts
+++ src/models/CurrentUser.ts
@@ -3,15 +3,34 @@ import { CurrentCompany, ICurrentCompany } from "./CurrentCompany";
 import { TCurrentUserAttributes } from "./hooks/queries";
 import { CurrentApplicant, TCurrentApplicant } from "./CurrentApplicant";
 import { CurrentAdmin, TCurrentAdmin } from "./CurrentAdmin";
+import { Role } from "./Role";
+import { RoleName } from "./Repositories";
 
-export const CurrentUser = (attributes: TCurrentUserAttributes): TCurrentUser => ({
-  ...attributes,
-  admin: attributes.admin && CurrentAdmin(attributes.admin),
-  company: attributes.company && CurrentCompany(attributes.company),
-  applicant: attributes.applicant && CurrentApplicant(attributes.applicant)
-});
+export const CurrentUser = (attributes: TCurrentUserAttributes): TCurrentUser => {
+  const currentUser = {
+    ...attributes,
+    admin: attributes.admin && CurrentAdmin(attributes.admin),
+    company: attributes.company && CurrentCompany(attributes.company),
+    applicant: attributes.applicant && CurrentApplicant(attributes.applicant),
+    getCurrentRole: () => {
+      let currentRole: RoleName = RoleName.Company;
+      if (currentUser.company) currentRole = RoleName.Company;
+      if (currentUser.admin) currentRole = RoleName.Admin;
+      if (currentUser.applicant) currentRole = RoleName.Applicant;
+      if (currentUser.admin && currentUser.applicant) currentRole = RoleName.Admin;
+      return new Role(currentRole);
+    }
+  };
+  return currentUser;
+};
 
-export type TCurrentUser = TGenericCurrentUser<TCurrentAdmin, TCurrentApplicant, ICurrentCompany>;
+export type TCurrentUser = TGenericCurrentUser<
+  TCurrentAdmin,
+  TCurrentApplicant,
+  ICurrentCompany
+> & {
+  getCurrentRole: () => Role;
+};
 
 export type TGenericCurrentUser<TAdmin, TApplicant, TCompany> = IUser & {
   admin?: TAdmin;
--- src/test/models/CurrentUser.test.ts
+++ src/test/models/CurrentUser.test.ts
@@ -1,6 +1,9 @@
 import { CurrentUser } from "$models/CurrentUser";
 import { ApprovalStatus } from "$interfaces/ApprovalStatus";
 import { Secretary } from "$interfaces/Secretary";
+import { RoleName, SessionStorageRepository } from "../../models/Repositories";
+import { calculateCurrentRole } from "../../models/calculateCurrentRole";
+import { Role } from "../../models/Role";
 
 describe("CurrentUser", () => {
   it("returns a valid current applicant user", () => {
@@ -49,5 +52,90 @@ describe("CurrentUser", () => {
     expect(currentUser?.admin).not.toBeUndefined();
     expect(currentUser?.company).toBeUndefined();
     expect(currentUser?.applicant).toBeUndefined();
+  });
+
+  describe("getCurrentRole", () => {
+    const commonAttributes = {
+      email: "companyUser@company.com",
+      name: "eric",
+      surname: "Clapton"
+    };
+
+    beforeEach(() => SessionStorageRepository.clear());
+
+    it("saves applicant as the current role", () => {
+      const currentUser = CurrentUser({
+        ...commonAttributes,
+        applicant: {
+          uuid: "4c925fdc-8fd4-47ed-9a24-fa81ed5cc9da",
+          approvalStatus: ApprovalStatus.pending
+        }
+      });
+      calculateCurrentRole(currentUser);
+      const role = SessionStorageRepository.getCurrentRole();
+      expect(role.isApplicantRole()).toBe(true);
+    });
+
+    it("saves company as the current role", () => {
+      const currentUser = CurrentUser({
+        ...commonAttributes,
+        company: {
+          uuid: "4c925fdc-8fd4-47ed-9a24-fa81ed5cc9da",
+          approvalStatus: ApprovalStatus.pending
+        }
+      });
+      calculateCurrentRole(currentUser);
+      const role = SessionStorageRepository.getCurrentRole();
+      expect(role.isCompanyRole()).toBe(true);
+    });
+
+    it("saves admin as the current role", () => {
+      const currentUser = CurrentUser({
+        ...commonAttributes,
+        admin: {
+          user: { uuid: "4c925fdc-8fd4-47ed-9a24-fa81ed5cc9da" },
+          secretary: Secretary.extension
+        }
+      });
+      calculateCurrentRole(currentUser);
+      const role = SessionStorageRepository.getCurrentRole();
+      expect(role.isAdminRole()).toBe(true);
+    });
+
+    it("saves admin as the current role if the current usr is admin and applicant", () => {
+      const currentUser = CurrentUser({
+        ...commonAttributes,
+        admin: {
+          user: { uuid: "4c925fdc-8fd4-47ed-9a24-fa81ed5cc9da" },
+          secretary: Secretary.extension
+        },
+        applicant: {
+          uuid: "4c925fdc-8fd4-47ed-9a24-fa81ed5cc9da",
+          approvalStatus: ApprovalStatus.pending
+        }
+      });
+      calculateCurrentRole(currentUser);
+      const role = SessionStorageRepository.getCurrentRole();
+      expect(role.isAdminRole()).toBe(true);
+    });
+
+    it("does not update the currentRole if there is already one", () => {
+      const applicantRole = new Role(RoleName.Applicant);
+      SessionStorageRepository.saveCurrentRole(applicantRole);
+      const currentUser = CurrentUser({
+        ...commonAttributes,
+        admin: {
+          user: { uuid: "4c925fdc-8fd4-47ed-9a24-fa81ed5cc9da" },
+          secretary: Secretary.extension
+        },
+        applicant: {
+          uuid: "4c925fdc-8fd4-47ed-9a24-fa81ed5cc9da",
+          approvalStatus: ApprovalStatus.pending
+        }
+      });
+      calculateCurrentRole(currentUser);
+      const role = SessionStorageRepository.getCurrentRole();
+      expect(role).toEqual(applicantRole);
+    });
   });
 });
