7787f5ff34202e735dbff67de8e05c5ceab94290
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-03 13:12:40 -0300

    Add condition for typescript

--- src/models/Router/Router.ts
+++ src/models/Router/Router.ts
@@ -7,8 +7,12 @@ import { SessionStorageRepository } from "$repositories";
 export const Router = {
   home: (currentUser: TCurrentUser) => {
     const currentRole = SessionStorageRepository.getCurrentRole();
-    if (currentRole.isCompanyRole()) return CompanyRouter.home(currentUser.company!);
-    if (currentRole.isApplicantRole()) return ApplicantRouter.home(currentUser.applicant!);
+    if (currentRole.isCompanyRole() && currentUser.company) {
+      return CompanyRouter.home(currentUser.company);
+    }
+    if (currentRole.isApplicantRole() && currentUser.applicant) {
+      return ApplicantRouter.home(currentUser.applicant);
+    }
     return RoutesBuilder.admin.home();
   }
 };
