5381e1138dad57a004c4d242d9b8768279e50b9d
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-05 06:02:10 -0300

    Remove unused hooks in the Filter component

--- src/pages/Admin/Applicants/Filter/container.tsx
+++ src/pages/Admin/Applicants/Filter/container.tsx
@@ -1,37 +1,26 @@
 import React, { FunctionComponent, useCallback } from "react";
-import { useHistory } from "react-router-dom";
 import { FormikHelpers } from "formik";
-import { useMyApplicantProfile, useUpdatePadron } from "$hooks";
 
 import { Filter } from "./component";
-import { Window } from "$components/Window";
 
 import { IFormValues } from "./interfaces";
 import { ApplicantType } from "$interfaces/Applicant";
 
 export const FilterContainer: FunctionComponent = () => {
-  const history = useHistory();
-  const applicant = useMyApplicantProfile();
-  const { updatePadron } = useUpdatePadron();
-
   const onSubmit = useCallback(
     async (_: IFormValues, __: FormikHelpers<IFormValues>) => undefined,
-    [history, updatePadron]
+    []
   );
 
-  const loading = !applicant;
-
   return (
-    <Window loading={loading}>
-      <Filter
-        onSubmit={onSubmit}
-        initialValues={{
-          _form: "",
-          careerCodes: [],
-          name: "",
-          applicantType: ApplicantType.student
-        }}
-      />
-    </Window>
+    <Filter
+      onSubmit={onSubmit}
+      initialValues={{
+        _form: "",
+        careerCodes: [],
+        name: "",
+        applicantType: ApplicantType.student
+      }}
+    />
   );
 };
