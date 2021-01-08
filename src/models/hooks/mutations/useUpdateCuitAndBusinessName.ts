bad6f80a4589a9aac4bcec83a27cc166ccb146ae
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-07 23:49:06 -0300

    Rename mutation from updateCuitAndBusinessName to updateCompanyCriticalAttributes

--- src/models/hooks/mutations/useUpdateCuitAndBusinessName.ts
+++ src/models/hooks/mutations/useUpdateCuitAndBusinessName.ts
@@ -1,9 +1,9 @@
-import { UPDATE_CUIT_AND_BUSINESS_NAME } from "$mutations";
+import { UPDATE_COMPANY_CRITICAL_ATTRIBUTES } from "$mutations";
 import { useMutation } from "$hooks";
 
 export const useUpdateCuitAndBusinessName = () => {
   const { mutation, ...result } = useMutation<IUseUpdateCuitAndBusinessName>(
-    UPDATE_CUIT_AND_BUSINESS_NAME
+    UPDATE_COMPANY_CRITICAL_ATTRIBUTES
   );
   return { updateCuitAndBusinessName: mutation, ...result };
 };
