b01c467ea8517ea170c4c46c097a10ebf6d263a5
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-11 18:52:47 -0300

    Add CardTag component

---
+++ src/components/CardTag/index.tsx
@@ -0,0 +1,12 @@
+import React, { FunctionComponent } from "react";
+import classNames from "classnames";
+import styles from "./styles.module.scss";
+
+export const AppliedTag: FunctionComponent<IComponentProps> = ({ color, label }) => (
+  <div className={classNames(styles.tag, styles[color])}>{label}</div>
+);
+
+interface IComponentProps {
+  label: string;
+  color: "green" | "blue";
+}
