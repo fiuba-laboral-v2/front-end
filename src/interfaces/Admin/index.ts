7e4c82018ef0e6156bde93ee3bc5baf792c0077d
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-14 14:34:47 -0300

    Add AdminStatus enum

--- src/interfaces/Admin/index.ts
+++ src/interfaces/Admin/index.ts
@@ -1,10 +1,16 @@
 import { Secretary } from "../Secretary";
 import { IFiubaUser } from "../User";
 
+export enum AdminStatus {
+  active = "Active",
+  deactivated = "Deactivated"
+}
+
 export interface IAdmin {
   uuid: string;
   user: IFiubaUser;
   secretary: Secretary;
+  status: AdminStatus;
   updatedAt: string;
   createdAt: string;
 }
