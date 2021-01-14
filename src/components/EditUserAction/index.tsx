72d5dbf3a4d267cd21989c854bbe95f9b8789b62
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-14 04:00:46 -0300

    Add DeactivateAccountAction in the admins table

--- src/components/EditUserAction/index.tsx
+++ src/components/EditUserAction/index.tsx
@@ -3,15 +3,20 @@ import EditIcon from "@material-ui/icons/Edit";
 import Tooltip from "@material-ui/core/Tooltip";
 import { IconButton } from "$components/IconButton";
 
-export const EditUserAction: FunctionComponent<IComponentProps> = ({ tooltipMessage, link }) => (
+export const EditUserAction: FunctionComponent<IComponentProps> = ({
+  className,
+  tooltipMessage,
+  link
+}) => (
   <Tooltip title={tooltipMessage}>
-    <div>
+    <div className={className}>
       <IconButton Icon={EditIcon} link={link} />
     </div>
   </Tooltip>
 );
 
 interface IComponentProps {
+  className?: string;
   tooltipMessage: string;
   link: string;
 }
--- src/pages/Admin/Admins/ListContentItem/Actions/component.tsx
+++ src/pages/Admin/Admins/ListContentItem/Actions/component.tsx
@@ -1,14 +1,25 @@
 import React, { FunctionComponent } from "react";
 import { EditUserAction } from "$components/EditUserAction";
+import { DeactivateAccountAction } from "$components/DeactivateAccountAction";
 import { TableActions } from "$components/TableActions";
 import { IComponentProps } from "./interfaces";
+import styles from "./styles.module.scss";
 
 export const Actions: FunctionComponent<IComponentProps> = ({
   className,
   editUserLink,
+  deactivateAccountLink,
   translations
 }) => (
   <TableActions className={className}>
-    <EditUserAction link={editUserLink} tooltipMessage={translations.editAdminTooltipMessage} />
+    <EditUserAction
+      className={styles.editUserAction}
+      link={editUserLink}
+      tooltipMessage={translations.editAdminTooltipMessage}
+    />
+    <DeactivateAccountAction
+      link={deactivateAccountLink}
+      tooltipMessage={translations.deactivateAccountLinkTooltipMessage}
+    />
   </TableActions>
 );
