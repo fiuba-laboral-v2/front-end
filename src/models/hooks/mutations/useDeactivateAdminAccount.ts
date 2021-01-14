988eca0961495017d08b9df8cb5a7c8334982357
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-14 15:41:51 -0300

    Refactor useDeactivateAdminAccount to update the cache instead of removing the entry

--- src/models/hooks/mutations/useDeactivateAdminAccount.ts
+++ src/models/hooks/mutations/useDeactivateAdminAccount.ts
@@ -1,6 +1,8 @@
 import { DEACTIVATE_ADMIN_ACCOUNT } from "$mutations";
+import { GET_ADMINS } from "$queries";
 import { IMutationOptions, useMutation } from "$hooks";
 import { IAdmin } from "$interfaces/Admin";
+import { IPaginatedResult } from "../queries/interfaces";
 
 export const useDeactivateAdminAccount = () => {
   const { mutation, ...result } = useMutation<IUseDeactivateAdminAccountInput, IResponseProps>(
@@ -14,7 +16,24 @@ export const useDeactivateAdminAccount = () => {
     mutation({
       variables,
       ...options,
-      update: cache => cache.evict({ id: `Admin:${variables?.uuid}` })
+      update: (cache, { data }) => {
+        cache.modify({
+          fields: {
+            getAdmins: async (existingAdmins: IPaginatedResult<IAdmin>) => {
+              await cache.writeQuery({
+                data: {
+                  getAdmins: {
+                    shouldFetchMore: true,
+                    results: [data?.deactivateAdminAccount]
+                  }
+                },
+                query: GET_ADMINS
+              });
+              return existingAdmins;
+            }
+          }
+        });
+      }
     });
 
   return { deactivateAdminAccount, ...result };
