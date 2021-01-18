3ce54aafc7f4cd508ebe2477a136f7caf71e21b4
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-18 20:46:22 -0300

    Remove unused FiubaServiceFetchErrorHandler

--- src/models/errorHandlers/FiubaServiceFetchErrorHandler.ts
+++
@@ -1,4 +0,0 @@
-import { IFormErrorHandlers } from "./formErrorHandlers";
-
-export const FiubaServiceFetchErrorHandler = ({ enqueueSnackbar }: IFormErrorHandlers) =>
-  enqueueSnackbar("Error en el servicio de autenticación de FIUBA", { variant: "error" });
