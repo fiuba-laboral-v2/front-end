6d5f2060fdd7f72caf4563661492723d7bb1e29f
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-24 04:55:27 -0300

    Add tooltip for CompanyLogoInput

--- src/components/CompanyLogoInput/index.tsx
+++ src/components/CompanyLogoInput/index.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from "react";
+import Tooltip from "@material-ui/core/Tooltip";
 import { ImageUpload } from "$components/ImageUpload";
 import { CompanyLogo } from "$components/CompanyLogo";
 import CloudUploadOutlinedIcon from "@material-ui/icons/CloudUploadOutlined";
@@ -10,27 +11,32 @@ export const CompanyLogoInput: FunctionComponent<IComponentProps> = ({
   setLogo,
   className
 }) => (
-  <div className={className}>
-    <ImageUpload onChange={images => setLogo(images[0].dataURL)}>
-      {({ imageList, onImageUpload, errors }) => (
-        <>
-          <div className={styles.inputContainer} onClick={imageList[0]?.onUpdate || onImageUpload}>
-            <div className={styles.overlay} />
-            <CloudUploadOutlinedIcon className={styles.uploadIcon} fontSize="large" />
-            <CompanyLogo
-              className={styles.logo}
-              logo={imageList[0]?.dataURL || initialValue}
-              size="extraLarge"
-              useDefaultIcon={false}
-            />
-          </div>
-          <FormHelperText className={styles.formHelperText} error>
-            {errors.maxFileSize ? "Tamaño máximo: 500MB" : " "}
-          </FormHelperText>
-        </>
-      )}
-    </ImageUpload>
-  </div>
+  <Tooltip title={"Icono de empresa"} placement="top-start">
+    <div className={className}>
+      <ImageUpload onChange={images => setLogo(images[0].dataURL)}>
+        {({ imageList, onImageUpload, errors }) => (
+          <>
+            <div
+              className={styles.inputContainer}
+              onClick={imageList[0]?.onUpdate || onImageUpload}
+            >
+              <div className={styles.overlay} />
+              <CloudUploadOutlinedIcon className={styles.uploadIcon} fontSize="large" />
+              <CompanyLogo
+                className={styles.logo}
+                logo={imageList[0]?.dataURL || initialValue}
+                size="extraLarge"
+                useDefaultIcon={false}
+              />
+            </div>
+            <FormHelperText className={styles.formHelperText} error>
+              {errors.maxFileSize ? "Tamaño máximo: 500MB" : " "}
+            </FormHelperText>
+          </>
+        )}
+      </ImageUpload>
+    </div>
+  </Tooltip>
 );
 
 export interface IComponentProps {
