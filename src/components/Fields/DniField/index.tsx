29dce1e298903926893836f2d658e4a28d34926b
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-30 19:35:59 -0300

    Use validateStringSpaces in the DniField

--- src/components/Fields/DniField/index.tsx
+++ src/components/Fields/DniField/index.tsx
@@ -1,14 +1,14 @@
 import React, { FunctionComponent } from "react";
 import { TextField } from "$components/Fields";
-import { validateIntegerInRange } from "validations-fiuba-laboral-v2";
+import { validateIntegerInRange, validateStringSpaces } from "validations-fiuba-laboral-v2";
 import { ITextFieldProps } from "../TextField";
 
-const validate = validateIntegerInRange({ min: { value: 0, include: false } });
+const validate = (value: string) => {
+  validateStringSpaces(value);
+  const dni = Number(value.replace(",", "."));
+  validateIntegerInRange({ min: { value: 0, include: false } })(dni);
+};
 
 export const DniField: FunctionComponent<ITextFieldProps> = props => (
-  <TextField
-    {...props}
-    autoComplete="username"
-    validator={value => validate(Number(value.replace(",", ".")))}
-  />
+  <TextField {...props} autoComplete="username" validator={validate} />
 );
