2fd587222f80701b0f93aa4630dc049659af3943
Sebastian Blanco <sebastian.e.blanco@gmail.com>
2021-01-30 23:04:40 -0300

    Update cache after creating an offer

--- src/models/hooks/mutations/useCreateOffer.ts
+++ src/models/hooks/mutations/useCreateOffer.ts
@@ -1,12 +1,38 @@
 import { CREATE_OFFER } from "$mutations";
-import { useMutation } from "$hooks";
-import { ICreateOffer } from "$interfaces/Offer";
+import { GET_MY_OFFERS } from "$queries";
+import { IMutationOptions, useMutation } from "$hooks";
+import { ICreateOffer, IOfferAttributes } from "$interfaces/Offer";
+import { IPaginatedResult } from "$hooks/queries/interfaces";
 
 export const useCreateOffer = () => {
-  const { mutation, ...result } = useMutation<ICreateOffer, ICreateOfferResponse>(CREATE_OFFER);
-  return { createOffer: mutation, ...result };
+  const { mutation, ...result } = useMutation<ICreateOffer, IResponse>(CREATE_OFFER);
+
+  const createOffer = ({ variables, ...options }: IMutationOptions<IResponse, ICreateOffer>) =>
+    mutation({
+      variables,
+      ...options,
+      update: (cache, { data }) =>
+        cache.modify({
+          fields: {
+            getMyOffers: async (existingOffers: IPaginatedResult<IOfferAttributes>) => {
+              await cache.writeQuery({
+                data: {
+                  getMyOffers: {
+                    shouldFetchMore: true,
+                    results: [data?.createOffer]
+                  }
+                },
+                query: GET_MY_OFFERS
+              });
+              return existingOffers;
+            }
+          }
+        })
+    });
+
+  return { createOffer, ...result };
 };
 
-interface ICreateOfferResponse {
-  createOffer: { uuid: string };
+interface IResponse {
+  createOffer: IOfferAttributes;
 }
